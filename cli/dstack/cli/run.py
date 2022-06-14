bd6e9f5f192dddbcc6ce6be6cf106a55902d2d84
peterschmidt85 <andrey.cheptsov@gmail.com>
2022-06-14 10:41:59 +0200

    - Major: Updated `dstack run` to support both workflows and providers (WIP)

--- cli/dstack/cli/run.py
+++ cli/dstack/cli/run.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import sys
@@ -16,103 +17,110 @@ from dstack.config import get_config, ConfigurationError
 
 
 def register_parsers(main_subparsers, main_parser):
-    parser = main_subparsers.add_parser("run", help="Run a workflow")
+    parser = main_subparsers.add_parser("run", help="Run a workflow or provider"
+                                        )
+    parser.add_argument("workflow_or_provider", metavar="(WORKFLOW | PROVIDER)", type=str,
+                        help="A name of a workflow or a provider")
+    parser.add_argument("--follow", "-f", help="Whether to continuously poll for new logs. By default, the command "
+                                               "will exit once there are no more logs to display. To exit from this "
+                                               "mode, use Control-C.", action="store_true")
+    parser.add_argument("vars", metavar="VARS", nargs=argparse.ZERO_OR_MORE,
+                        help="Override workflow workflow_variables")
+    parser.add_argument("args", metavar="ARGS", nargs=argparse.ZERO_OR_MORE, help="Override provider arguments")
     workflows_yaml = load_workflows()
-    variables = load_variables()
+    workflow_variables = load_variables()
     workflows = (workflows_yaml.get("workflows") or []) if workflows_yaml is not None else []
     workflow_names = [w.get("name") for w in workflows]
-    workflow_helps = {w.get("name"): w.get("help") for w in workflows}
-    subparsers = parser.add_subparsers()
-    for workflow_name in workflow_names:
-        workflow_parser = subparsers.add_parser(workflow_name, help=workflow_helps.get(workflow_name))
-        workflow_parser.add_argument("--follow", "-f",
-                                     help="Whether to continuously poll and print logs of the workflow. "
-                                          "By default, the command doesn't print logs. To exit from this "
-                                          "mode, use Control-C.", action="store_true")
+    workflow_providers = {w.get("name"): w.get("provider") for w in workflows}
 
-        # TODO: Walk the graph of dependencies and only include the variables from dependencies
-        # Because our workflow may depend on other workflows, we allow overriding all variables
-        workflow_variables = {}
-        for section in variables:
-            v_variables = variables.get(section)
-            for name in v_variables:
-                workflow_variables[name] = v_variables[name]
-                workflow_parser.add_argument("--" + name,
-                                             help=f"by default, the value is {pretty_print_variable_value(v_variables[name])}",
-                                             type=str,
-                                             nargs="?")
+    def default_run_func(args):
+        try:
+            validate(workflows_yaml, yaml.load(workflows_schema_yaml, Loader=yaml.FullLoader))
+            repo_url, repo_branch, repo_hash, repo_diff = load_repo_data()
+            dstack_config = get_config()
+            # # TODO: Support non-default profiles
+            profile = dstack_config.get_profile("default")
 
-        def run_workflow(_workflow_name):
-            def _run_workflow(args):
-                if len(workflows) > 0:
-                    try:
-                        validate(workflows_yaml, yaml.load(workflows_schema_yaml, Loader=yaml.FullLoader))
-                        repo_url, repo_branch, repo_hash, repo_diff = load_repo_data()
-                        dstack_config = get_config()
-                        # TODO: Support non-default profiles
-                        profile = dstack_config.get_profile("default")
+            provider_args = args.vars + args.args + args.unknown
 
-                        # TODO: Support large repo_diff
-                        headers = {
-                            "Content-Type": f"application/json; charset=utf-8"
-                        }
-                        if profile.token is not None:
-                            headers["Authorization"] = f"Bearer {profile.token}"
+            workflow_name = None
+            provider_name = None
+            provider_branch = None
+            variables = {}
+            if args.workflow_or_provider in workflow_names:
+                workflow_name = args.workflow_or_provider
+                provider_name = workflow_providers[workflow_name]
 
-                        def fix_variable_name(name: str):
-                            alternative_name = name.replace("_", "-")
-                            if alternative_name in workflow_variables and name not in workflow_variables:
-                                return alternative_name
-                            else:
-                                return name
-
-                        _variables = {fix_variable_name(k): v for (k, v) in vars(args).items() if
-                                      k != "func" and k != "follow" and v is not None}
-                        data = {
-                            "workflow_name": _workflow_name,
-                            "repo_url": repo_url,
-                            "repo_branch": repo_branch,
-                            "repo_hash": repo_hash,
-                            "variables": _variables
-                        }
-                        if repo_diff:
-                            data["repo_diff"] = repo_diff
-                        data_bytes = json.dumps(data).encode("utf-8")
-                        response = requests.request(method="POST", url=f"{profile.server}/runs/submit",
-                                                    data=data_bytes,
-                                                    headers=headers, verify=profile.verify)
-                        if response.status_code == 200:
-                            runs_func(Namespace(run_name=response.json().get("run_name"), last=1))
-                            if args.follow:
-                                logs_func(Namespace(run_name_or_job_id=response.json().get("run_name"),
-                                                    follow=True, since="1d"))
-                        elif response.status_code == 404 and response.json().get("message") == "repo not found":
-                            sys.exit("Call 'dstack init' first")
-                        else:
-                            response.raise_for_status()
-
-                    except ConfigurationError:
-                        sys.exit(f"Call 'dstack config' first")
-                    except InvalidGitRepositoryError:
-                        sys.exit(f"{os.getcwd()} is not a Git repo")
-                    except ValidationError as e:
-                        sys.exit(f"There a syntax error in {os.getcwd()}/.dstack/workflows.yaml:\n\n{e}")
+                for idx, arg in enumerate(provider_args[:]):
+                    if arg.startswith('--'):
+                        arg_name = arg[2:]
+                        if workflow_variables.get(workflow_name) and arg_name in workflow_variables[workflow_name] \
+                                and idx < len(provider_args) - 1:
+                            variables[arg_name] = provider_args[idx + 1]
+                            del provider_args[idx]
+                            del provider_args[idx]
+                        if workflow_variables.get("global") and arg_name in workflow_variables["global"] \
+                                and idx < len(provider_args) - 1:
+                            variables[arg_name] = provider_args[idx + 1]
+                            del provider_args[idx]
+                            del provider_args[idx]
+            else:
+                if "@" in args.workflow_or_provider:
+                    tokens = args.workflow_or_provider.split('@', maxsplit=1)
+                    provider_name = tokens[0]
+                    provider_branch = tokens[1]
                 else:
-                    sys.exit(f"No workflows defined in {os.getcwd()}/.dstack/workflows.yaml")
+                    provider_name = args.workflow_or_provider
 
-            return _run_workflow
+                if workflow_variables.get("global"):
+                    for idx, arg in enumerate(provider_args[:]):
+                        if arg.startswith('--'):
+                            arg_name = arg[2:]
+                            if arg_name in workflow_variables["global"] \
+                                    and idx < len(provider_args) - 1:
+                                variables[arg_name] = provider_args[idx + 1]
+                                del provider_args[idx]
+                                del provider_args[idx]
 
-        workflow_parser.set_defaults(func=run_workflow(workflow_name))
+            # # TODO: Support large repo_diff
+            headers = {
+                "Content-Type": f"application/json; charset=utf-8"
+            }
+            if profile.token is not None:
+                headers["Authorization"] = f"Bearer {profile.token}"
 
-    def default_run_workflow(_: Namespace):
-        parser.print_help()
-        exit(1)
+            data = {
+                "workflow_name": workflow_name,
+                "provider_name": provider_name,
+                "provider_branch": provider_branch,
+                "provider_args": provider_args,
+                "repo_url": repo_url,
+                "repo_branch": repo_branch,
+                "repo_hash": repo_hash,
+                "workflow_variables": variables
+            }
+            if repo_diff:
+                data["repo_diff"] = repo_diff
 
-    parser.set_defaults(func=default_run_workflow)
+            data_bytes = json.dumps(data).encode("utf-8")
+            response = requests.request(method="POST", url=f"{profile.server}/runs/submit",
+                                        data=data_bytes,
+                                        headers=headers, verify=profile.verify)
+            if response.status_code == 200:
+                runs_func(Namespace(run_name=response.json().get("run_name"), last=1))
+                if args.follow:
+                    logs_func(Namespace(run_name_or_job_id=response.json().get("run_name"),
+                                        follow=True, since="1d"))
+            elif response.status_code == 404 and response.json().get("message") == "repo not found":
+                sys.exit("Call 'dstack init' first")
+            else:
+                response.raise_for_status()
 
+        except ConfigurationError:
+            sys.exit(f"Call 'dstack config' first")
+        except InvalidGitRepositoryError:
+            sys.exit(f"{os.getcwd()} is not a Git repo")
+        except ValidationError as e:
+            sys.exit(f"There a syntax error in {os.getcwd()}/.dstack/workflows.yaml:\n\n{e}")
 
-def pretty_print_variable_value(obj):
-    if type(obj) is str:
-        return f"\"{obj}\""
-    else:
-        return str(obj)
+    parser.set_defaults(func=default_run_func)
