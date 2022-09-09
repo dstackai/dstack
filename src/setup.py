316eca9fd39586b71d22c02186ecec29929290e1
peterschmidt85 <andrey.cheptsov@gmail.com>
2022-09-09 16:11:52 +0200

    - Refactoring: Bundling dashboard dist into the CLI on build

--- src/setup.py
+++ src/setup.py
@@ -20,7 +20,7 @@ setup(
     author="Andrey Cheptsov",
     author_email="andrey@dstack.ai",
     packages=find_packages(),
-    package_data={'dstack.dashboard': ['statics/*', 'statics/**/*']},
+    package_data={'dstack.dashboard': ['statics/*', 'statics/**/*', 'statics/**/**/*']},
     include_package_data=True,
     scripts=[],
     entry_points={
