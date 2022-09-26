c36f0c424343032c23b3355c4935992e36d3b5d4
peterschmidt85 <andrey.cheptsov@gmail.com>
2022-09-26 16:02:45 +0200

    - Refactoring: Do not load finished jobs in `dstack ps`, use job heads

--- src/dstack/aws/runs.py
+++ src/dstack/aws/runs.py
@@ -18,37 +18,43 @@ def create_run(s3_client: BaseClient, logs_client: BaseClient, bucket_name: str,
     return run_name
 
 
-def _create_run(ec2_client: BaseClient, repo_user_name, repo_name, job: Job, include_request_heads: bool) -> RunHead:
-    app_heads = list(map(lambda a: AppHead(job.job_id, a.app_name), job.app_specs)) if job.app_specs else None
-    artifact_heads = list(
-        map(lambda a: ArtifactHead(job.job_id, a.artifact_path), job.artifact_specs)) if job.artifact_specs else None
+def _create_run(ec2_client: BaseClient, s3_client: BaseClient, bucket_name: str, job_head: JobHead,
+                include_request_heads: bool) -> RunHead:
+    app_heads = list(
+        map(lambda app_name: AppHead(job_head.job_id, app_name), job_head.app_names)) if job_head.app_names else None
+    artifact_heads = list(map(lambda artifact_path: ArtifactHead(job_head.job_id, artifact_path),
+                              job_head.artifact_paths)) if job_head.artifact_paths else None
     request_heads = None
-    if include_request_heads and job.status.is_unfinished():
+    if include_request_heads and job_head.status.is_unfinished():
         if request_heads is None:
             request_heads = []
+        job = jobs.get_job(s3_client, bucket_name, job_head.repo_user_name, job_head.repo_name, job_head.job_id)
         request_head = runners.get_request_head(ec2_client, job)
         request_heads.append(request_head)
-    run_head = RunHead(repo_user_name, repo_name, job.run_name, job.workflow_name, job.provider_name,
-                       artifact_heads or None, job.status, job.submitted_at, job.tag_name,
-                       app_heads, request_heads)
+    run_head = RunHead(job_head.repo_user_name, job_head.repo_name, job_head.run_name, job_head.workflow_name,
+                       job_head.provider_name, artifact_heads or None, job_head.status, job_head.submitted_at,
+                       job_head.tag_name, app_heads, request_heads)
     return run_head
 
 
-def _update_run(ec2_client: BaseClient, run: RunHead, job: Job, include_request_heads: bool):
-    run.submitted_at = min(run.submitted_at, job.submitted_at)
-    if job.artifact_specs:
+def _update_run(ec2_client: BaseClient, s3_client: BaseClient, bucket_name: str, run: RunHead, job_head: JobHead,
+                include_request_heads: bool):
+    run.submitted_at = min(run.submitted_at, job_head.submitted_at)
+    if job_head.artifact_paths:
         if run.artifact_heads is None:
             run.artifact_heads = []
-        run.artifact_heads.extend(list(map(lambda a: ArtifactHead(job.job_id, a.artifact_path), job.artifact_specs)))
-    if job.app_specs:
+        run.artifact_heads.extend(
+            list(map(lambda artifact_path: ArtifactHead(job.job_id, artifact_path), job_head.artifact_paths)))
+    if job_head.app_names:
         if run.app_heads is None:
             run.app_heads = []
-        run.app_heads.extend(list(map(lambda a: AppHead(job.job_id, a.app_name), job.app_specs)))
-    if job.status.is_unfinished():
-        run.status = job.status
+        run.app_heads.extend(list(map(lambda app_name: AppHead(job.job_id, app_name), job_head.app_names)))
+    if job_head.status.is_unfinished():
+        run.status = job_head.status
         if include_request_heads:
             if run.request_heads is None:
                 run.request_heads = []
+            job = jobs.get_job(s3_client, bucket_name, job_head.repo_user_name, job_head.repo_name, job_head.job_id)
             request_head = runners.get_request_head(ec2_client, job)
             run.request_heads.append(request_head)
 
@@ -57,14 +63,12 @@ def get_run_heads(ec2_client: BaseClient, s3_client: BaseClient, bucket_name: st
                   job_heads: List[JobHead], include_request_heads: bool) -> List[RunHead]:
     runs_by_id = {}
     for job_head in job_heads:
-        job = jobs.get_job(s3_client, bucket_name, repo_user_name, repo_name, job_head.job_id)
-        if job:
-            run_id = ','.join([job.run_name, job.workflow_name or ''])
-            if run_id not in runs_by_id:
-                runs_by_id[run_id] = _create_run(ec2_client, repo_user_name, repo_name, job, include_request_heads)
-            else:
-                run = runs_by_id[run_id]
-                _update_run(ec2_client, run, job, include_request_heads)
+        run_id = ','.join([job_head.run_name, job_head.workflow_name or ''])
+        if run_id not in runs_by_id:
+            runs_by_id[run_id] = _create_run(ec2_client, s3_client, bucket_name, job_head, include_request_heads)
+        else:
+            run = runs_by_id[run_id]
+            _update_run(ec2_client, s3_client, bucket_name, run, job_head, include_request_heads)
     return sorted(list(runs_by_id.values()), key=lambda r: r.submitted_at, reverse=True)
 
 
