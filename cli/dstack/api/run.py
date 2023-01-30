528594d1954edae1223a5d0f17457909a2d163cb
Ivan Bokov <bokov@selectel.ru>
2023-01-30 19:48:06 +0300

    - [CLI]: Fixed ps command. Now correct showing last run if all runs are finished

--- cli/dstack/api/run.py
+++ cli/dstack/api/run.py
@@ -1,7 +1,24 @@
 from typing import List, Union
 from dstack.backend import Backend
 from dstack.core.run import RunHead
+from dstack.core.repo import RepoData
 from dstack.api.repo import load_repo_data
+
+
+def get_runs(repo_data: RepoData, backend: Backend, run_name: str = "", all: bool = False) -> List[RunHead]:
+    runs = []
+    job_heads = backend.list_job_heads(repo_data, run_name)
+    runs_backend = backend.get_run_heads(repo_data, job_heads)
+    for run in runs_backend:
+        run.backend_name = backend.name
+        runs.append(run)
+    if not all:
+        unfinished = any(run.status.is_unfinished() for run in runs)
+        if unfinished:
+            runs = list(filter(lambda r: r.status.is_unfinished(), runs))
+        else:
+            runs = runs[:1]
+    return runs
 
 
 def list_runs(backends: Union[List[Backend], Backend], run_name: str = "", all: bool = False) -> List[RunHead]:
@@ -9,22 +26,8 @@ def list_runs(backends: Union[List[Backend], Backend], run_name: str = "", all: 
     runs = []
     if type(backends) == list:
         for backend in backends:
-            job_heads = backend.list_job_heads(repo_data, run_name)
-            runs_backend = backend.get_run_heads(repo_data, job_heads)
-            for run in runs_backend:
-                run.backend_name = backend.name
-                runs.append(run)
+           runs = runs + [run for run in get_runs(repo_data, backend, run_name, all)]
     else:
-        job_heads = backends.list_job_heads(repo_data, run_name)
-        runs_backend = backends.get_run_heads(repo_data, job_heads)
-        for run in runs_backend:
-            run.backend_name = backends.name
-            runs.append(run)
-    if not all:
-        unfinished = any(run.status.is_unfinished() for run in runs)
-        if unfinished:
-            runs = list(filter(lambda r: r.status.is_unfinished(), runs))
-        else:
-            runs = runs[:1]
+        runs = runs + [run for run in get_runs(repo_data, backends, run_name, all)]
     runs = reversed(runs)
     return runs
