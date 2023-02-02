65b1f23cdb7f35356f20a2c19c84e705c8b3bd3b
Victor Skvortsov <vds003@gmail.com>
2023-02-02 14:48:41 +0500

    Remove dstack artifacts command

--- cli/dstack/cli/handlers.py
+++ cli/dstack/cli/handlers.py
@@ -1,6 +1,5 @@
 from dstack.cli.commands import BasicCommand
 import dstack.cli.commands.run
-import dstack.cli.commands.artifacts
 import dstack.cli.commands.config
 
 # import dstack.cli.commands.hub
@@ -8,7 +7,6 @@ import dstack.cli.commands.init
 import dstack.cli.commands.logs
 import dstack.cli.commands.ls
 import dstack.cli.commands.ps
-
 import dstack.cli.commands.pull
 import dstack.cli.commands.rm
 import dstack.cli.commands.run
