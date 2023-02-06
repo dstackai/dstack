3ed555a7f56ebd12327888241be1a3b58fbe3c90
Victor Skvortsov <vds003@gmail.com>
2023-02-06 17:58:51 +0500

    Fix listing of local artifacts in subdirectories (#169)
    * Fix listing of local artifacts in subdirectories
    
    * Fix uploading of miltiple artifacts

--- cli/dstack/cli/commands/push/__init__.py
+++ cli/dstack/cli/commands/push/__init__.py
@@ -58,13 +58,15 @@ class PushCommand(BasicCommand):
         run_artifacts = local_backend.list_run_artifact_files(
             repo_address=repo_data, run_name=run_name
         )
-        for artifact in run_artifacts:
+        unique_artifacts = {(a.job_id, a.name) for a in run_artifacts}
+
+        for job_id, artifact_name in unique_artifacts:
             remote_backend.upload_job_artifact_files(
                 repo_address=repo_data,
-                job_id=artifact.job_id,
-                artifact_name=artifact.name,
+                job_id=job_id,
+                artifact_name=artifact_name,
                 local_path=Path(
-                    local_backend.get_artifacts_path(repo_data), artifact.job_id, artifact.name
+                    local_backend.get_artifacts_path(repo_data), job_id, artifact_name
                 ),
             )
 
