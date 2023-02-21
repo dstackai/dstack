1ebb76d7a9821447708c3c40deee1a670dde4c41
Victor Skvortsov <vds003@gmail.com>
2023-02-21 10:27:40 +0500

    Implement gcp storage for runner

--- runner/go.mod
+++ runner/go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/google/uuid v1.3.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.1
 	github.com/urfave/cli/v2 v2.3.0
 	go.uber.org/atomic v1.4.0
 	golang.org/x/crypto v0.6.0
@@ -23,6 +23,7 @@ require (
 )
 
 require (
+	cloud.google.com/go/storage v1.29.0
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.15
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.15 // indirect
@@ -35,10 +36,8 @@ require (
 	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6 // indirect
-	google.golang.org/grpc v1.38.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	google.golang.org/api v0.106.0
+	gopkg.in/yaml.v3 v3.0.1
 	modernc.org/sqlite v1.20.3
 )
 
---
+++ runner/internal/backend/gcp/storage.go
@@ -0,0 +1,88 @@
+package gcp
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"cloud.google.com/go/storage"
+	"github.com/dstackai/dstack/runner/internal/gerrors"
+	"google.golang.org/api/iterator"
+)
+
+type GCPStorage struct {
+	client      *storage.Client
+	bucket      *storage.BucketHandle
+	project     string
+	bucketName string
+}
+
+func NewGCPStorage(project, bucketName string) (*GCPStorage, error) {
+	ctx := context.TODO()
+	client, _ := storage.NewClient(ctx)
+	bucket := client.Bucket(bucketName)
+	if bucket == nil {
+		return nil, gerrors.New("Cannot access bucket")
+	}
+	return &GCPStorage{
+		client:      client,
+		bucket:      bucket,
+		project:     project,
+		bucketName: bucketName,
+	}, nil
+}
+
+func (gstorage *GCPStorage) GetFile(ctx context.Context, key string) ([]byte, error) {
+	obj := gstorage.bucket.Object(key)
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		return nil, gerrors.Wrap(err)
+	}
+	defer reader.Close()
+	buffer := new(bytes.Buffer)
+	io.Copy(buffer, reader)
+	return buffer.Bytes(), nil
+}
+
+func (gstorage *GCPStorage) PutFile(ctx context.Context, key string, contents []byte) error {
+	obj := gstorage.bucket.Object(key)
+	writer := obj.NewWriter(ctx)
+	reader := bytes.NewReader(contents)
+	io.Copy(writer, reader)
+	return writer.Close()
+}
+
+func (gstorage *GCPStorage) ListFile(ctx context.Context, prefix string) ([]string, error) {
+	query := &storage.Query{Prefix: prefix}
+	names := make([]string, 0)
+	it := gstorage.bucket.Objects(ctx, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, gerrors.Wrap(err)
+		}
+		names = append(names, attrs.Name)
+	}
+	return names, nil
+}
+
+func (gstorage *GCPStorage) DeleteFile(ctx context.Context, key string) error {
+	obj := gstorage.bucket.Object(key)
+	err := obj.Delete(ctx)
+	return gerrors.Wrap(err)
+}
+
+func (gstorage *GCPStorage) RenameFile(ctx context.Context, oldKey, newKey string) error {
+	src := gstorage.bucket.Object(oldKey)
+	dst := gstorage.bucket.Object(newKey)
+	copier := dst.CopierFrom(src)
+	_, err := copier.Run(ctx)
+	if err != nil {
+		return gerrors.Wrap(err)
+	}
+	err = src.Delete(ctx)
+	return gerrors.Wrap(err)
+}
