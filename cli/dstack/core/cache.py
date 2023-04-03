0b53328c7b9568d4ebb3443cbbb1a6778aca9e24
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-04-03 17:37:25 +0400

    CacheSpec: strip dot relative

--- cli/dstack/core/cache.py
+++ cli/dstack/core/cache.py
@@ -1,5 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class CacheSpec(BaseModel):
     path: str
+
+    @validator("path")
+    def remove_dot_relative(cls, v: str) -> str:
+        return v.lstrip("./")
