c376b0ea50436e4529c27c805279e3aa2606c2e5
Oleg Vavilov <vavilovolegik@gmail.com>
2023-05-22 21:53:42 +0300

    Added stopping, aborting, deleting runs

--- hub/src/services/run.types.ts
+++ hub/src/services/run.types.ts
@@ -4,3 +4,16 @@ export type RunsRequestParams = {
     run_name?: string;
     include_request_heads?: boolean;
 };
+
+export type DeleteRunsRequestParams = {
+    name: IProject['project_name'];
+    repo_id?: string;
+    run_names: IRun['run_name'][];
+};
+
+export type StopRunsRequestParams = {
+    name: IProject['project_name'];
+    repo_id?: string;
+    run_names: IRun['run_name'][];
+    abort: boolean;
+};
