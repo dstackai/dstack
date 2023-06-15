a24772e668af9541d376865387c5db8883caf721
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-06-15 15:23:07 +0400

    Manual AMI build: pick the latest runner build from the current branch

--- runner/ami/packer/provisioners/get-dstack-runner.sh
+++ runner/ami/packer/provisioners/get-dstack-runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+RUNNER_VERSION=${RUNNER_VERSION:-latest}
+
 function install_fuse {
 sudo apt install s3fs -y
 if [ -e "/etc/fuse.conf" ]; then
@@ -11,7 +13,7 @@ else
 fi
 }
 function install_stgn {
-  sudo curl --output /usr/local/bin/dstack-runner "https://dstack-runner-downloads-stgn.s3.eu-west-1.amazonaws.com/latest/binaries/dstack-runner-linux-amd64"
+  sudo curl --output /usr/local/bin/dstack-runner "https://dstack-runner-downloads-stgn.s3.eu-west-1.amazonaws.com/${RUNNER_VERSION}/binaries/dstack-runner-linux-amd64"
   sudo chmod +x /usr/local/bin/dstack-runner
   dstack-runner --version
   install_fuse
