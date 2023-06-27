e2772ffa5b398a604d306bbeaad5bb8abfa3338a
Victor Skvortsov <vds003@gmail.com>
2023-06-27 11:41:59 +0500

    Set up lambda for prod and stage

--- cli/dstack/_internal/scripts/setup_lambda.sh
+++ cli/dstack/_internal/scripts/setup_lambda.sh
@@ -15,14 +15,14 @@ function install_stgn {
 }
 
 function install_prod {
-  sudo curl --output /usr/local/bin/dstack-runner "https://dstack-runner-downloads.s3.eu-west-1.amazonaws.com/latest/binaries/dstack-runner-linux-amd64"
+  sudo curl --output /usr/local/bin/dstack-runner "https://dstack-runner-downloads.s3.eu-west-1.amazonaws.com/${RUNNER_VERSION}/binaries/dstack-runner-linux-amd64"
   sudo chmod +x /usr/local/bin/dstack-runner
   dstack-runner --version
 }
 
 install_nvidia_docker_runtime
 
-if [[ $DSTACK_STAGE == "PROD" ]]; then
+if [[ $ENVIRONMENT == "PROD" ]]; then
   install_prod
 else
   install_stgn
