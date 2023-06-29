02e4acf945beba16167be0cd939d557c1fa81147
Victor Skvortsov <vds003@gmail.com>
2023-06-29 09:22:37 +0500

    Support dstack[lambda] install

--- setup.py
+++ setup.py
@@ -27,6 +27,74 @@ def get_long_description():
     )
 
 
+BASE_DEPS = [
+    "pyyaml",
+    "requests",
+    "gitpython",
+    "tqdm",
+    "jsonschema",
+    "python-dateutil",
+    "paramiko",
+    "git-url-parse",
+    "rich",
+    "rich-argparse",
+    "fastapi",
+    "starlette>=0.26.0",
+    "uvicorn",
+    "pydantic",
+    "sqlalchemy[asyncio]>=2.0.0",
+    "websocket-client",
+    "cursor",
+    "simple-term-menu",
+    "py-cpuinfo",
+    "pygtail",
+    "packaging",
+    "aiosqlite",
+    "apscheduler",
+    "alembic>=1.10.2",
+    "typing-extensions>=4.0.0",
+    "file-read-backwards>=3.0.0",
+    "psutil>=5.0.0",
+    "cryptography",
+    "grpcio>=1.50",  # indirect
+    "filelock",
+    "watchfiles",
+]
+
+AWS_DEPS = [
+    "boto3",
+    "botocore",
+]
+
+AZURE_DEPS = [
+    "azure-identity>=1.12.0",
+    "azure-keyvault-secrets>=4.6.0",
+    "azure-storage-blob>=12.15.0",
+    "azure-monitor-query>=1.2.0",
+    "azure-mgmt-subscription>=3.1.1",
+    "azure-mgmt-compute>=29.1.0",
+    "azure-mgmt-network==23.0.0b2",
+    "azure-mgmt-resource>=22.0.0",
+    "azure-mgmt-authorization>=3.0.0",
+    "azure-mgmt-storage>=21.0.0",
+    "azure-mgmt-keyvault>=10.1.0",
+    "azure-mgmt-loganalytics==13.0.0b6",
+    "azure-mgmt-msi",
+    "azure-mgmt-monitor",
+    "azure-graphrbac",
+]
+
+GCP_DEPS = [
+    "google-auth>=2.3.0",  # indirect
+    "google-cloud-storage>=2.0.0",
+    "google-cloud-compute>=1.5.0",
+    "google-cloud-secret-manager>=2.0.0",
+    "google-cloud-logging>=2.0.0",
+    "google-api-python-client>=2.80.0",
+]
+
+LAMBDA_DEPS = AWS_DEPS
+
 setup(
     name="dstack",
     version=get_version(),
@@ -56,69 +124,12 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
-    install_requires=[
-        "pyyaml",
-        "requests",
-        "gitpython",
-        "tqdm",
-        "jsonschema",
-        "python-dateutil",
-        "paramiko",
-        "git-url-parse",
-        "rich",
-        "rich-argparse",
-        "fastapi",
-        "starlette>=0.26.0",
-        "uvicorn",
-        "pydantic",
-        "sqlalchemy[asyncio]>=2.0.0",
-        "websocket-client",
-        "cursor",
-        "simple-term-menu",
-        "py-cpuinfo",
-        "pygtail",
-        "packaging",
-        "aiosqlite",
-        "apscheduler",
-        "alembic>=1.10.2",
-        "typing-extensions>=4.0.0",
-        "file-read-backwards>=3.0.0",
-        "psutil>=5.0.0",
-        "cryptography",
-        "grpcio>=1.50",  # indirect
-        "filelock",
-        "watchfiles",
-    ],
+    install_requires=BASE_DEPS,
     extras_require={
-        "aws": [
-            "boto3",
-            "botocore",
-        ],
-        "azure": [
-            "azure-identity>=1.12.0",
-            "azure-keyvault-secrets>=4.6.0",
-            "azure-storage-blob>=12.15.0",
-            "azure-monitor-query>=1.2.0",
-            "azure-mgmt-subscription>=3.1.1",
-            "azure-mgmt-compute>=29.1.0",
-            "azure-mgmt-network==23.0.0b2",
-            "azure-mgmt-resource>=22.0.0",
-            "azure-mgmt-authorization>=3.0.0",
-            "azure-mgmt-storage>=21.0.0",
-            "azure-mgmt-keyvault>=10.1.0",
-            "azure-mgmt-loganalytics==13.0.0b6",
-            "azure-mgmt-msi",
-            "azure-mgmt-monitor",
-            "azure-graphrbac",
-        ],
-        "gcp": [
-            "google-auth>=2.3.0",  # indirect
-            "google-cloud-storage>=2.0.0",
-            "google-cloud-compute>=1.5.0",
-            "google-cloud-secret-manager>=2.0.0",
-            "google-cloud-logging>=2.0.0",
-            "google-api-python-client>=2.80.0",
-        ],
+        "aws": AWS_DEPS,
+        "azure": AZURE_DEPS,
+        "gcp": GCP_DEPS,
+        "lambda": LAMBDA_DEPS,
     },
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
