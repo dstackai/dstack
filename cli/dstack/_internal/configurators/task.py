80af4942d46bb3b23c2cb7da2465169f59d16b97
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-08-01 16:04:19 +0400

    Do not require sshd in task configuration for custom docker images

--- cli/dstack/_internal/configurators/task.py
+++ cli/dstack/_internal/configurators/task.py
@@ -32,9 +32,6 @@ class TaskConfigurator(JobConfiguratorWithPorts):
 
     def setup(self) -> List[str]:
         commands = []
-        if self.conf.image:
-            commands += self.sshd.get_required_commands()
-        commands += self.sshd.get_setup_commands()
         commands += self.conf.setup
         return commands
 
