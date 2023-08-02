d620ba2cf2e9d77eb53c647d0792101dc5f1d6aa
peterschmidt85 <andrey.cheptsov@gmail.com>
2023-08-02 10:44:28 +0200

    - [CLI]: Bug: Doesn't run a dev environment if code is not configured in PATH #612

--- cli/dstack/_internal/configurators/extensions/vscode.py
+++ cli/dstack/_internal/configurators/extensions/vscode.py
@@ -23,36 +23,32 @@ class VSCodeDesktop(IDEExtension):
 
     def get_install_commands(self) -> List[str]:
         commands = []
-        if self.version is None:
-            return
-        url = (
-            f"https://update.code.visualstudio.com/commit:{self.version}/server-linux-$arch/stable"
-        )
-        archive = "vscode-server-linux-$arch.tar.gz"
-        target = f'~/.vscode-server/bin/"{self.version}"'
-        commands.extend(
-            [
-                f'if [ $(uname -m) = "aarch64" ]; then arch="arm64"; else arch="x64"; fi',
-                f"mkdir -p /tmp",
-                f'wget -q --show-progress "{url}" -O "/tmp/{archive}"',
-                f"mkdir -vp {target}",
-                f'tar --no-same-owner -xz --strip-components=1 -C {target} -f "/tmp/{archive}"',
-                f'rm "/tmp/{archive}"',
-            ]
-        )
-        if self.extensions:
-            extensions = " ".join(f'--install-extension "{name}"' for name in self.extensions)
-            commands.append(f'PATH="$PATH":{target}/bin code-server {extensions}')
+        if self.version is not None:
+            url = f"https://update.code.visualstudio.com/commit:{self.version}/server-linux-$arch/stable"
+            archive = "vscode-server-linux-$arch.tar.gz"
+            target = f'~/.vscode-server/bin/"{self.version}"'
+            commands.extend(
+                [
+                    f'if [ $(uname -m) = "aarch64" ]; then arch="arm64"; else arch="x64"; fi',
+                    f"mkdir -p /tmp",
+                    f'wget -q --show-progress "{url}" -O "/tmp/{archive}"',
+                    f"mkdir -vp {target}",
+                    f'tar --no-same-owner -xz --strip-components=1 -C {target} -f "/tmp/{archive}"',
+                    f'rm "/tmp/{archive}"',
+                ]
+            )
+            if self.extensions:
+                extensions = " ".join(f'--install-extension "{name}"' for name in self.extensions)
+                commands.append(f'PATH="$PATH":{target}/bin code-server {extensions}')
         return commands
 
     def get_install_if_not_found_commands(self) -> List[str]:
-        if self.version is None:
-            return
         commands = []
-        install_commands = " && ".join(self.get_install_commands())
-        commands.append(
-            f'if [ ! -d ~/.vscode-server/bin/"{self.version}" ]; then {install_commands}; fi'
-        )
+        if self.version is not None:
+            install_commands = " && ".join(self.get_install_commands())
+            commands.append(
+                f'if [ ! -d ~/.vscode-server/bin/"{self.version}" ]; then {install_commands}; fi'
+            )
         return commands
 
     def get_print_readme_commands(self) -> List[str]:
