a22bd8f2ddb2e5a2990e7371bfa1340a7cf67178
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-08-07 17:35:17 +0400

    Shutdown scheduler on server stop

--- cli/dstack/_internal/hub/background/__init__.py
+++ cli/dstack/_internal/hub/background/__init__.py
@@ -7,7 +7,7 @@ from apscheduler.triggers.interval import IntervalTrigger
 from dstack._internal.hub.background.tasks.resubmit_jobs import resubmit_jobs
 
 
-def start_background_tasks():
+def start_background_tasks() -> AsyncIOScheduler:
     def _error_listner(event: JobExecutionEvent):
         if isinstance(event.exception, asyncio.CancelledError):
             scheduler.shutdown()
@@ -16,3 +16,4 @@ def start_background_tasks():
     scheduler.add_job(resubmit_jobs, IntervalTrigger(seconds=60))
     scheduler.add_listener(_error_listner, EVENT_JOB_ERROR)
     scheduler.start()
+    return scheduler
