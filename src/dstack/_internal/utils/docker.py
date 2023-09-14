fe8500c88dce7ac35917a19da8ca1653552c5063
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-09-14 18:45:33 +0400

    Refactor parse_image_name, add ghcr.io support

--- src/dstack/_internal/utils/docker.py
+++ src/dstack/_internal/utils/docker.py
@@ -1,7 +1,23 @@
+from dataclasses import dataclass
 from enum import Enum
-from typing import Tuple
+from typing import Optional
 
 import requests
+
+manifests_media_types = [
+    "application/vnd.oci.image.index.v1+json",
+    "application/vnd.oci.image.manifest.v1+json",
+    "application/vnd.docker.distribution.manifest.v2+json",
+    "application/vnd.docker.distribution.manifest.list.v2+json",
+]
+
+
+@dataclass(frozen=True)
+class DockerImage:
+    registry: Optional[str]
+    repo: str
+    tag: str
+    digest: Optional[str]
 
 
 class DockerPlatform(str, Enum):
@@ -11,90 +27,108 @@ class DockerPlatform(str, Enum):
 
 class DockerRegistryClient:
     def __init__(self):
-        self.repo = None
-        self.token = None
+        self.registry: Optional[str] = None
+        self.repo: Optional[str] = None
+        self.s = requests.Session()
 
-    def auth(self, repo: str):
-        auth_host = "auth.docker.io"  # todo ghcr.io
-        service = "registry.docker.io"
-        r = requests.get(
-            f"https://{auth_host}/token",
-            params={
-                "service": service,
-                "scope": f"repository:{repo}:pull",
-            },
-        )
-        r.raise_for_status()
+    def auth(self, repo: str, *, registry: Optional[str] = None, token: Optional[str] = None):
+        self.registry = registry
         self.repo = repo
-        self.token = r.json()["token"]
 
-    def manifests(
-        self, tag: str = "latest", *, accept="application/vnd.docker.distribution.manifest.v2+json"
-    ) -> dict:
-        registry_host = "registry-1.docker.io"
-        r = requests.get(
+        if not token:
+            auth_host = registry
+            params = {"scope": f"repository:{repo}:pull"}
+            if not registry:
+                auth_host = "auth.docker.io"
+                params["service"] = "registry.docker.io"
+
+            r = requests.get(f"https://{auth_host}/token", params=params)
+            r.raise_for_status()
+            token = r.json()["token"]
+        self.s.headers = {"Authorization": f"Bearer {token}"}
+
+    def manifests(self, tag: str = "latest", *, accept: Optional[str] = None) -> dict:
+        registry_host = self.registry or "registry-1.docker.io"
+        r = self.s.get(
             f"https://{registry_host}/v2/{self.repo}/manifests/{tag}",
-            headers={
-                "Authorization": f"Bearer {self.token}",
-                "Accept": accept,
-            },
+            headers={"Accept": accept or ",".join(manifests_media_types)},
         )
         r.raise_for_status()
         return r.json()
 
     def blobs(self, digest: str) -> dict:
-        registry_host = "registry-1.docker.io"
-        r = requests.get(
-            f"https://{registry_host}/v2/{self.repo}/blobs/{digest}",
-            headers={
-                "Authorization": f"Bearer {self.token}",
-            },
-        )
+        registry_host = self.registry or "registry-1.docker.io"
+        r = self.s.get(f"https://{registry_host}/v2/{self.repo}/blobs/{digest}")
         r.raise_for_status()
         return r.json()
 
 
-def get_image_config(image: str, platform: DockerPlatform = DockerPlatform.x86) -> dict:
-    _, repo, tag = parse_image_name(image)
-    # todo ghcr.io
+def get_image_config(
+    image: str, *, platform: DockerPlatform = DockerPlatform.x86, token: Optional[str] = None
+) -> dict:
+    image = parse_image_name(image)
     client = DockerRegistryClient()
-    client.auth(repo)
+    client.auth(image.repo, registry=image.registry, token=token)
 
-    manifest = client.manifests(tag)
-    if "config" not in manifest:
+    manifest = client.manifests(image.digest or image.tag)
+    if "config" not in manifest:  # manifest index/list, pick the right platform
         for m in manifest["manifests"]:
             if m["platform"]["architecture"] == platform.value:
                 manifest = client.manifests(m["digest"], accept=m["mediaType"])
                 break
+        else:
+            raise RuntimeError("No manifest for the specified platform")
 
     return client.blobs(manifest["config"]["digest"])
 
 
-def parse_image_name(image: str) -> Tuple[str, str, str]:
+def parse_image_name(image: str) -> DockerImage:
     """
     :param image: docker image name
-    :return: registry host, repository, tag
+    :return: registry host, repo, tag, digest
 
     >>> parse_image_name("ubuntu:22.04")
-    ('', 'library/ubuntu', '22.04')
+    DockerImage(registry=None, repo='library/ubuntu', tag='22.04', digest=None)
     >>> parse_image_name("dstackai/miniforge:py3.9-0.2")
-    ('', 'dstackai/miniforge', 'py3.9-0.2')
+    DockerImage(registry=None, repo='dstackai/miniforge', tag='py3.9-0.2', digest=None)
     >>> parse_image_name("ghcr.io/dstackai/miniforge")
-    ('ghcr.io', 'dstackai/miniforge', 'latest')
+    DockerImage(registry='ghcr.io', repo='dstackai/miniforge', tag='latest', digest=None)
     >>> parse_image_name("dstackai/miniforge@sha256:a4ba18a847a172a248d68faf6689e69fae4779b90b250211b79a26d21ddd6a15")
-    ('', 'dstackai/miniforge', 'sha256:a4ba18a847a172a248d68faf6689e69fae4779b90b250211b79a26d21ddd6a15')
+    DockerImage(registry=None, repo='dstackai/miniforge', tag='latest', digest='sha256:a4ba18a847a172a248d68faf6689e69fae4779b90b250211b79a26d21ddd6a15')
     """
-    parts = image.split("/")
-    if len(parts) == 1 or (
-        parts[0] != "localhost" and ":" not in parts[0] and "." not in parts[0]
-    ):
-        host = ""
-        repo_tag = "library/" + parts[0] if len(parts) == 1 else "/".join(parts)
-    else:
-        host = parts[0]
-        repo_tag = "/".join(parts[1:])
-    if "@" in repo_tag:
-        repo, tag = repo_tag.split("@")
-    else:
-        repo, tag = repo_tag.split(":") if ":" in repo_tag else (repo_tag, "latest")
-    return host, repo, tag
+
+    digest = None
+    if "@" in image.split("/")[-1]:
+        image, digest = image.rsplit("@", maxsplit=1)
+
+    tag = "latest"
+    if ":" in image.split("/")[-1]:  # avoid detecting port as a tag
+        image, tag = image.rsplit(":", maxsplit=1)
+
+    registry = None
+    components = image.split("/")
+    if len(components) == 1:  # default registry, official image
+        repo = "library/" + components[0]
+    elif not is_host(components[0]):  # default registry, custom image
+        repo = "/".join(components)
+    else:  # custom registry
+        registry = components[0]
+        repo = "/".join(components[1:])
+
+    return DockerImage(registry, repo, tag, digest)
+
+
+def is_host(s: str) -> bool:
+    """
+    >>> is_host("localhost")
+    True
+    >>> is_host("localhost:5000")
+    True
+    >>> is_host("ghcr.io")
+    True
+    >>> is_host("127.0.0.1")
+    True
+    >>> is_host("dstackai")
+    False
+    """
+    return s == "localhost" or ":" in s or "." in s
