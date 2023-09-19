dd27d9a45924c4e5e91d3e11fc3d26aaa1facf5d
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-09-19 11:00:40 +0400

    Reimplement get_instance_candidates

--- src/dstack/_internal/core/backends/base/__init__.py
+++ src/dstack/_internal/core/backends/base/__init__.py
@@ -1,8 +1,10 @@
 from abc import abstractmethod
-from datetime import datetime
-from typing import List, Optional
+from typing import List
 
 from dstack._internal.core.backends.base.compute import Compute
+from dstack._internal.core.models.instances import InstanceOfferWithAvailability
+from dstack._internal.core.models.profiles import BackendType, SpotPolicy
+from dstack._internal.core.models.runs import Requirements
 
 
 class Backend:
@@ -11,3 +13,13 @@ class Backend:
     @abstractmethod
     def compute(self) -> Compute:
         pass
+
+    def get_instance_candidates(
+        self, requirements: Requirements, spot_policy: SpotPolicy
+    ) -> List[InstanceOfferWithAvailability]:
+        offers = self.compute().get_offers(requirements)
+        if spot_policy == SpotPolicy.SPOT:
+            return [i for i in offers if i.instance.resources.spot]
+        if spot_policy == SpotPolicy.ONDEMAND:
+            return [i for i in offers if not i.instance.resources.spot]
+        return offers
