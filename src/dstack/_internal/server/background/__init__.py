538cff2a3c4c00789339cd4641c3e18bb2a2a17b
Victor Skvortsov <vds003@gmail.com>
2023-10-02 11:53:12 +0500

    Trigger process_pending_jobs

--- src/dstack/_internal/server/background/__init__.py
+++ src/dstack/_internal/server/background/__init__.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
+from dstack._internal.server.background.tasks.process_pending_jobs import process_pending_jobs
 from dstack._internal.server.background.tasks.process_running_jobs import process_running_jobs
 from dstack._internal.server.background.tasks.process_submitted_jobs import process_submitted_jobs
 
@@ -9,5 +10,6 @@ def start_background_tasks() -> AsyncIOScheduler:
     scheduler = AsyncIOScheduler()
     scheduler.add_job(process_submitted_jobs, IntervalTrigger(seconds=2))
     scheduler.add_job(process_running_jobs, IntervalTrigger(seconds=2))
+    scheduler.add_job(process_pending_jobs, IntervalTrigger(seconds=10))
     scheduler.start()
     return scheduler
