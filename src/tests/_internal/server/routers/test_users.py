73ced7189613d7f5ed03657b0981c297f875b806
Victor Skvortsov <vds003@gmail.com>
2023-11-08 10:24:00 +0300

    Fix 500 server error on creating users with existing usernames (#766)
    * Fix server 500 error on creating user with existing username
    
    * Add delete users endpoint
    
    * Hide all users from non-admin users
    
    * Fix global admin access

--- src/tests/_internal/server/routers/test_users.py
+++ src/tests/_internal/server/routers/test_users.py
@@ -1,8 +1,14 @@
+from unittest.mock import patch
+from uuid import UUID
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from dstack._internal.core.models.users import GlobalRole
 from dstack._internal.server.main import app
+from dstack._internal.server.models import UserModel
 from dstack._internal.server.testing.common import create_user, get_auth_headers
 
 client = TestClient(app)
@@ -76,3 +82,83 @@ class TestGetUser:
             "global_role": other_user.global_role,
             "creds": {"token": "1234"},
         }
+
+
+class TestCreateUser:
+    def test_returns_40x_if_not_authenticated(self):
+        response = client.post("/api/users/create")
+        assert response.status_code in [401, 403]
+
+    @pytest.mark.asyncio
+    async def test_creates_user(self, test_db, session: AsyncSession):
+        user = await create_user(name="admin", session=session)
+        with patch("uuid.uuid4") as uuid_mock:
+            uuid_mock.return_value = UUID("1b0e1b45-2f8c-4ab6-8010-a0d1a3e44e0e")
+            response = client.post(
+                "/api/users/create",
+                headers=get_auth_headers(user.token),
+                json={
+                    "username": "test",
+                    "global_role": GlobalRole.USER,
+                },
+            )
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": "1b0e1b45-2f8c-4ab6-8010-a0d1a3e44e0e",
+            "username": "test",
+            "global_role": "user",
+        }
+        res = await session.execute(select(UserModel).where(UserModel.name == "test"))
+        assert len(res.scalars().all()) == 1
+
+    @pytest.mark.asyncio
+    async def test_return_400_if_username_taken(self, test_db, session: AsyncSession):
+        user = await create_user(name="admin", session=session)
+        with patch("uuid.uuid4") as uuid_mock:
+            uuid_mock.return_value = UUID("1b0e1b45-2f8c-4ab6-8010-a0d1a3e44e0e")
+            response = client.post(
+                "/api/users/create",
+                headers=get_auth_headers(user.token),
+                json={
+                    "username": "test",
+                    "global_role": GlobalRole.USER,
+                },
+            )
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": "1b0e1b45-2f8c-4ab6-8010-a0d1a3e44e0e",
+            "username": "test",
+            "global_role": "user",
+        }
+        with patch("uuid.uuid4") as uuid_mock:
+            uuid_mock.return_value = UUID("1b0e1b45-2f8c-4ab6-8010-a0d1a3e44e0e")
+            response = client.post(
+                "/api/users/create",
+                headers=get_auth_headers(user.token),
+                json={
+                    "username": "test",
+                    "global_role": GlobalRole.USER,
+                },
+            )
+        assert response.status_code == 400
+        res = await session.execute(select(UserModel).where(UserModel.name == "test"))
+        assert len(res.scalars().all()) == 1
+
+
+class TestDeleteUsers:
+    def test_returns_40x_if_not_authenticated(self):
+        response = client.post("/api/users/delete")
+        assert response.status_code in [401, 403]
+
+    @pytest.mark.asyncio
+    async def test_deletes_users(self, test_db, session: AsyncSession):
+        admin = await create_user(name="admin", session=session)
+        user = await create_user(name="test", session=session)
+        response = client.post(
+            "/api/users/delete",
+            headers=get_auth_headers(admin.token),
+            json={"users": [user.name]},
+        )
+        assert response.status_code == 200
+        res = await session.execute(select(UserModel).where(UserModel.name == user.name))
+        assert len(res.scalars().all()) == 0
