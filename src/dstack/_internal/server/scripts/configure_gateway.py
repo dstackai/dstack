109f3deec63ef53ed3ff6899dae5948b65bdb665
Victor Skvortsov <vds003@gmail.com>
2023-12-11 11:54:24 +0500

    Add custom global nginx config

--- src/dstack/_internal/server/scripts/configure_gateway.py
+++ src/dstack/_internal/server/scripts/configure_gateway.py
@@ -10,6 +10,74 @@ from typing import Any, Dict, List, Tuple, Union
 
 owner = "www-data"
 
+nginx_global_conf_path = Path("/etc/nginx/nginx.conf")
+
+nginx_global_conf = """
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+	worker_connections 768;
+	# multi_accept on;
+}
+
+http {
+
+	##
+	# Basic Settings
+	##
+
+	sendfile on;
+	tcp_nopush on;
+	types_hash_max_size 2048;
+	# server_tokens off;
+
+	server_names_hash_bucket_size 128;
+	# server_name_in_redirect off;
+
+	include /etc/nginx/mime.types;
+	default_type application/octet-stream;
+
+	##
+	# SSL Settings
+	##
+
+	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+	ssl_prefer_server_ciphers on;
+
+	##
+	# Logging Settings
+	##
+
+	access_log /var/log/nginx/access.log;
+	error_log /var/log/nginx/error.log;
+
+	##
+	# Gzip Settings
+	##
+
+	gzip on;
+
+	# gzip_vary on;
+	# gzip_proxied any;
+	# gzip_comp_level 6;
+	# gzip_buffers 16 8k;
+	# gzip_http_version 1.1;
+	# gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+	##
+	# Virtual Host Configs
+	##
+
+	include /etc/nginx/conf.d/*.conf;
+	include /etc/nginx/sites-enabled/*;
+}
+
+"""
+
+
 # payload schema
 # {
 #     "authorized_key": "ssh-rsa ...",
@@ -104,6 +172,9 @@ def main():
                 }
             )
         service["conf_path"].write_text(format_nginx_conf(conf))
+
+    nginx_global_conf_path.write_text(nginx_global_conf)
+
     print(json.dumps(sock_paths))
     reload_nginx()
 
