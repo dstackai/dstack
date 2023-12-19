bc79356f5dfc6c05833cfbb7de0583365f460eec
peterschmidt85 <andrey.cheptsov@gmail.com>
2023-12-19 10:36:01 +0100

    - Allow to configure disk volume and type via resources #135 (part 3)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ BASE_DEPS = [
     "docker>=6.0.0",
     "dnspython",
     "grpcio>=1.50",  # indirect
-    "gpuhunt @ git+https://github.com/dstackai/gpuhunt@30-support-disk-size",
+    "gpuhunt==0.0.3rc1",
     "sentry-sdk[fastapi]",
 ]
 
