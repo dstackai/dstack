2bf0db05318f924a9583093e361a929d0c9027de
Andrey Cheptsov <54148038+peterschmidt85@users.noreply.github.com>
2023-12-19 08:54:44 +0100

    Update the CUDA version in the base Docker image to 12.1.0 #805 (#806)

--- packer/provisioners/docker-image-with-cuda.sh
+++ packer/provisioners/docker-image-with-cuda.sh
@@ -3,10 +3,10 @@
 set -e
 
 IMAGES="
- dstackai/base:py3.11-${IMAGE_VERSION}-cuda-11.8
- dstackai/base:py3.10-${IMAGE_VERSION}-cuda-11.8
- dstackai/base:py3.9-${IMAGE_VERSION}-cuda-11.8
- dstackai/base:py3.8-${IMAGE_VERSION}-cuda-11.8
+ dstackai/base:py3.11-${IMAGE_VERSION}-cuda-12.1
+ dstackai/base:py3.10-${IMAGE_VERSION}-cuda-12.1
+ dstackai/base:py3.9-${IMAGE_VERSION}-cuda-12.1
+ dstackai/base:py3.8-${IMAGE_VERSION}-cuda-12.1
 "
 echo "START pull image"
 for img in $IMAGES; do
