56c3fe0e4ea6bd251cb9a17d60526d422f04b31b
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2024-01-30 13:19:20 +0100

    Add docstrings for Resources constructors

--- src/dstack/api/_public/resources.py
+++ src/dstack/api/_public/resources.py
@@ -27,6 +27,19 @@ def Resources(
     shm_size: Optional[MemoryLike] = None,
     disk: Optional[DiskLike] = None,
 ) -> ResourcesSpec:
+    """
+    Creates required resources specification.
+
+    Args:
+        cpu (Optional[Range[int]]): The number of CPUs
+        memory (Optional[Range[Memory]]): The size of RAM memory (e.g., `"16GB"`)
+        gpu (Optional[GPUSpec]): The GPU spec
+        shm_size (Optional[Range[Memory]]): The of shared memory (e.g., `"8GB"`). If you are using parallel communicating processes (e.g., dataloaders in PyTorch), you may need to configure this.
+        disk (Optional[DiskSpec]): The disk spec
+
+    Returns:
+        resources specification
+    """
     return ResourcesSpec.parse_obj(
         ResourcesSpecSchema(
             cpu=cpu,
@@ -46,6 +59,19 @@ def GPU(
     total_memory: Optional[MemoryRangeLike] = None,
     compute_capability: Optional[ComputeCapabilityLike] = None,
 ) -> GPUSpec:
+    """
+    Creates GPU specification.
+
+    Args:
+        name (Optional[List[str]]): The name of the GPU (e.g., `"A100"` or `"H100"`)
+        count (Optional[Range[int]]): The number of GPUs
+        memory (Optional[Range[Memory]]): The size of a single GPU memory (e.g., `"16GB"`)
+        total_memory (Optional[Range[Memory]]): The total size of all GPUs memory (e.g., `"32GB"`)
+        compute_capability (Optional[float]): The minimum compute capability of the GPU (e.g., `7.5`)
+
+    Returns:
+        GPU specification
+    """
     return GPUSpec.parse_obj(
         GPUSpecSchema(
             name=name,
@@ -61,6 +87,15 @@ def Disk(
     *,
     size: MemoryRangeLike,
 ) -> DiskSpec:
+    """
+    Creates disk specification.
+
+    Args:
+        size (Range[Memory]): The size of the disk (e.g., `"100GB"`)
+
+    Returns:
+        disk specification
+    """
     return DiskSpec.parse_obj(
         DiskSpecSchema(
             size=size,
