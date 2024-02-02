d9f313db61696d3a4b3b87ee8ee5d5f9a6b6ed5b
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2024-02-02 12:54:52 +0100

    Generate REST API reference for docs

---
+++ scripts/docs/gen_openapi_reference.py
@@ -0,0 +1,17 @@
+"""
+Generates OpenAPI schema from dstack server app.
+"""
+import json
+
+import mkdocs_gen_files
+
+import dstack.version
+from dstack._internal.server.main import app
+
+app.servers = [
+    {"url": "http://localhost:3000", "description": "Local server"},
+    # {"url": "https://cloud.dstack.ai", "description": "Managed server"},
+]
+app.version = dstack.version.__version__ or "0.0.0"
+with mkdocs_gen_files.open("docs/reference/api/rest/openapi.json", "w") as f:
+    json.dump(app.openapi(), f)
