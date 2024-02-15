2bc0b2e872f619c3f1c311843935c3ed685546d5
Sergey Mezentsev <sergeyme@dstack.ai>
2024-02-15 16:00:03 +0300

    Revert "Enable golangci-lint presets: format and comment"
    This reverts commit a47660a8711a4fa1e0125af6b78a6faf1152ad7e.

--- runner/consts/consts.go
+++ runner/consts/consts.go
@@ -2,25 +2,21 @@ package consts
 
 import "time"
 
-const (
-	DSTACK_DIR_PATH    string = ".dstack"
-	CONFIG_FILE_NAME   string = "backend.yaml"
-	RUNNER_FILE_NAME   string = "runner.yaml"
-	TMP_DIR_PATH              = DSTACK_DIR_PATH + "/tmp"
-	USER_ARTIFACTS_DIR        = "artifacts"
-	FUSE_DIR                  = "fuse"
-	RUNS_DIR                  = "runs"
-)
+const DSTACK_DIR_PATH string = ".dstack"
+const CONFIG_FILE_NAME string = "backend.yaml"
+const RUNNER_FILE_NAME string = "runner.yaml"
+const TMP_DIR_PATH = DSTACK_DIR_PATH + "/tmp"
+const USER_ARTIFACTS_DIR = "artifacts"
+const FUSE_DIR = "fuse"
+const RUNS_DIR = "runs"
 
-const (
-	FILE_LOCK_FULL_DOWNLOAD   = ".lock.full"
-	SETUP_COMPLETED_FILE_NAME = ".setup_completed"
-)
+const FILE_LOCK_FULL_DOWNLOAD = ".lock.full"
+const SETUP_COMPLETED_FILE_NAME = ".setup_completed"
 
 // ServerUrl A default build-time variable. The value is overridden via ldflags.
 var ServerUrl = "https://api.stgn.dstack.ai"
 
-// var ServerUrl = "https://api.dstack.ai"
+//var ServerUrl = "https://api.dstack.ai"
 
 // GPU constants
 const (
@@ -36,17 +32,13 @@ const (
 	EXPOSE_PORT_END   = 4000
 )
 
-const (
-	MAX_ATTEMPTS = 10
-	DELAY_TRY    = 6 * time.Second
-)
+const MAX_ATTEMPTS = 10
+const DELAY_TRY = 6 * time.Second
 
 const DELAY_READ_STATUS = 5 * time.Second
 
-const (
-	REPO_HTTPS_URL = "https://%s/%s/%s.git"
-	REPO_GIT_URL   = "git@%s:%s/%s.git"
-)
+const REPO_HTTPS_URL = "https://%s/%s/%s.git"
+const REPO_GIT_URL = "git@%s:%s/%s.git"
 
 const (
 	TERMINATE_POLICY = "terminate"
