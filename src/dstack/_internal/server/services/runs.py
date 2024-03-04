b737bf6db22ff59a0241840eafac4b711ca187fe
Sergey Mezentsev <thebits@yandex.ru>
2024-03-04 13:02:25 +0300

    Fix python-lint CI

--- src/dstack/_internal/server/services/runs.py
+++ src/dstack/_internal/server/services/runs.py
@@ -89,7 +89,7 @@ BACKENDS_WITH_CREATE_INSTANCE_SUPPORT = [
     BackendType.DATACRUNCH,
     BackendType.GCP,
     BackendType.LAMBDA,
-    BackendType.TENSORDOCK
+    BackendType.TENSORDOCK,
 ]
 
 logger = get_logger(__name__)
