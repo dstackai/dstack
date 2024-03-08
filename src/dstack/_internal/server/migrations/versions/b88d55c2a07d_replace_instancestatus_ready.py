fff4ceee86a59eaf0ce9da4a5f735a5cc3e0b08e
Sergey Mezentsev <sergeyme@dstack.ai>
2024-03-08 10:27:15 +0300

    Fix InstanceStatus migration

--- src/dstack/_internal/server/migrations/versions/b88d55c2a07d_replace_instancestatus_ready.py
+++ src/dstack/_internal/server/migrations/versions/b88d55c2a07d_replace_instancestatus_ready.py
@@ -5,8 +5,6 @@ Revises: ed0ca30e13bb
 Create Date: 2024-02-28 06:15:32.172109
 
 """
-import sqlalchemy as sa
-from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "b88d55c2a07d"
@@ -16,8 +14,8 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.execute(sa.sql.text("UPDATE instances SET status = 'READY' WHERE status = 'IDLE'"))
+    pass
 
 
 def downgrade() -> None:
-    op.execute(sa.sql.text("UPDATE instances SET status = 'IDLE' WHERE status = 'READY'"))
+    pass
