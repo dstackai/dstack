cff6523de909e442c2cca16fdb0846207284b726
Victor Skvortsov <vds003@gmail.com>
2024-03-22 14:52:48 +0500

    Fix test_migrations relative path

--- src/tests/_internal/server/test_migrations.py
+++ src/tests/_internal/server/test_migrations.py
@@ -1,11 +1,15 @@
+from pathlib import Path
+
 import pytest
-from alembic.command import check, upgrade, util
+from alembic.command import check, upgrade
 from alembic.config import Config
+from alembic.util.exc import CommandError
 from sqlalchemy import create_engine
 
 
-def test_a_database_migration_needs_to_be_added(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.chdir("src/dstack/_internal/server")
+def test_no_database_migration_needs_to_be_added(monkeypatch: pytest.MonkeyPatch):
+    server_dir = Path(__file__).parent.joinpath("../../../dstack/_internal/server").resolve()
+    monkeypatch.chdir(server_dir)
 
     alembic_cfg = Config("alembic.ini")
     alembic_cfg.attributes["connection"] = create_engine("sqlite://").connect()
@@ -13,5 +17,5 @@ def test_a_database_migration_needs_to_be_added(monkeypatch: pytest.MonkeyPatch)
     try:
         upgrade(alembic_cfg, "head")
         check(alembic_cfg)
-    except util.CommandError as e:
+    except CommandError as e:
         pytest.fail(str(e))
