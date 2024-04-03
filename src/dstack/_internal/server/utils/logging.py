643f382a9e1883cfd3eec79470e00e9d73a00081
peterschmidt85 <andrey.cheptsov@gmail.com>
2024-04-03 18:40:29 +0200

    Minor improvements of dstack server output #1087

--- src/dstack/_internal/server/utils/logging.py
+++ src/dstack/_internal/server/utils/logging.py
@@ -5,6 +5,7 @@ import sys
 
 from pythonjsonlogger import jsonlogger
 
+from dstack._internal.cli.utils.common import DstackRichHandler, console
 from dstack._internal.server import settings
 
 
@@ -18,25 +19,28 @@ class AsyncioCancelledErrorFilter(logging.Filter):
 
 
 def configure_logging():
-    default_formatter = "standard"
-    formatters = {
-        default_formatter: logging.Formatter(
-            fmt="%(levelname)s %(asctime)s.%(msecs)03d %(name)s %(message)s",
-            datefmt="%Y-%m-%dT%H:%M:%S",
-        ),
-        "json": jsonlogger.JsonFormatter(
-            "%(asctime)s %(name)s %(levelname)s %(message)s",
-            json_ensure_ascii=False,
-            rename_fields={"name": "logger", "asctime": "timestamp"},
-        ),
-    }
+    default_formatter = "rich"
     log_format = os.getenv("DSTACK_SERVER_LOG_FORMAT", default_formatter)
-    formatter = formatters.get(log_format, formatters[default_formatter])
-
-    root_logger = logging.getLogger(None)
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.addFilter(AsyncioCancelledErrorFilter())
+    if log_format == "rich":
+        formatter = logging.Formatter(fmt="%(message)s", datefmt="[%X]")
+        handler = DstackRichHandler(console=console)
+    else:
+        formatters = {
+            "standard": logging.Formatter(
+                fmt="%(levelname)s %(asctime)s.%(msecs)03d %(name)s %(message)s",
+                datefmt="%Y-%m-%dT%H:%M:%S",
+            ),
+            "json": jsonlogger.JsonFormatter(
+                "%(asctime)s %(name)s %(levelname)s %(message)s",
+                json_ensure_ascii=False,
+                rename_fields={"name": "logger", "asctime": "timestamp"},
+            ),
+        }
+        formatter = formatters.get(log_format, formatters[default_formatter])
+        handler = logging.StreamHandler(stream=sys.stdout)
     handler.setFormatter(formatter)
+    handler.addFilter(AsyncioCancelledErrorFilter())
+    root_logger = logging.getLogger(None)
     root_logger.addHandler(handler)
     root_logger.setLevel(settings.ROOT_LOG_LEVEL)
     dstack_logger = logging.getLogger("dstack")
