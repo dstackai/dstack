f2c51987ed5cdbba91575858312751e1d665e7eb
peterschmidt85 <andrey.cheptsov@gmail.com>
2024-04-12 14:47:33 +0200

    The new default `rich` server logger format emits unreadable logs #1102 (bug-fix)

--- src/dstack/_internal/cli/utils/rich.py
+++ src/dstack/_internal/cli/utils/rich.py
@@ -3,101 +3,95 @@ from datetime import datetime
 from typing import TYPE_CHECKING, Callable, Iterable, List, Optional, Union
 
 from rich.logging import RichHandler
-from rich.text import Text, TextType
+from rich.text import Text
 from rich.traceback import Traceback
 
 if TYPE_CHECKING:
     from rich.console import Console, ConsoleRenderable, RenderableType
-    from rich.table import Table
 
 FormatTimeCallable = Callable[[datetime], Text]
 
 
-class LogRender:
-    def __init__(
-        self,
-        show_time: bool = True,
-        show_level: bool = True,
-        show_path: bool = True,
-        time_format: Union[str, FormatTimeCallable] = "[%x %X]",
-        omit_repeated_times: bool = True,
-        level_width: Optional[int] = 8,
-    ) -> None:
-        self.show_time = show_time
-        self.show_level = show_level
-        self.show_path = show_path
-        self.time_format = time_format
-        self.omit_repeated_times = omit_repeated_times
-        self.level_width = level_width
-        self._last_time: Optional[Text] = None
+class DstackLogRender:
+    _last_time: Optional[Text] = None
 
     def __call__(
         self,
         console: "Console",
         message_renderable: Iterable["ConsoleRenderable"],
         log_time: Optional[datetime] = None,
-        time_format: Optional[Union[str, FormatTimeCallable]] = None,
-        level: TextType = "",
+        time_format: Union[str, FormatTimeCallable] = "[%x %X]",
+        level: Text = Text(),
         path: Optional[str] = None,
         line_no: Optional[int] = None,
         link_path: Optional[str] = None,
-    ) -> "Table":
+    ) -> "ConsoleRenderable":
         from rich.containers import Renderables
-        from rich.table import Table
 
-        output = Table.grid(padding=(0, 1))
-        output.expand = True
-        if self.show_time:
-            output.add_column(style="log.time")
-        if self.show_level:
-            output.add_column(style="log.level", width=self.level_width)
-        output.add_column(ratio=1, overflow="ignore")
         row: List["RenderableType"] = []
-        if self.show_time:
-            log_time = log_time or console.get_datetime()
-            time_format = time_format or self.time_format
-            if callable(time_format):
-                log_time_display = time_format(log_time)
-            else:
-                log_time_display = Text(log_time.strftime(time_format))
-            if log_time_display == self._last_time and self.omit_repeated_times:
-                row.append(Text(" " * len(log_time_display)))
-            else:
-                row.append(log_time_display)
-                self._last_time = log_time_display
-        if self.show_level:
-            row.append(level)
+        log_time = log_time or console.get_datetime()
+        time_format = time_format
+        if callable(time_format):
+            log_time_display = time_format(log_time)
+        else:
+            log_time_display = Text(log_time.strftime(time_format), end=" ", style="log.time")
+        if log_time_display == self._last_time:
+            row.append(Text(" " * len(log_time_display)))
+        else:
+            row.append(log_time_display)
+            self._last_time = log_time_display
+        level.end = " " * (9 - len(level))
+        level.style = "log.level"
+        row.append(level)
 
-        merged_message_renderable = []
-        if self.show_path and path:
-            path_text = Text(style="log.path", end=" ")
-            path_text.append(path, style=f"link file://{link_path}" if link_path else "")
-            if line_no:
-                path_text.append(":")
-                path_text.append(
-                    f"{line_no}",
-                    style=f"link file://{link_path}#{line_no}" if link_path else "",
-                )
-            merged_message_renderable.append(path_text)
-        merged_message_renderable.extend(message_renderable)
-        row.append(Renderables(merged_message_renderable))
+        path_text = Text(style="log.path", overflow="fold", end=" ")
+        path_text.append(path, style=f"link file://{link_path}" if link_path else "")
+        if line_no:
+            path_text.append(":")
+            path_text.append(
+                f"{line_no}",
+                style=f"link file://{link_path}#{line_no}" if link_path else "",
+            )
+        row.append(path_text)
+        row.extend(message_renderable)
 
-        output.add_row(*row)
-        return output
+        return Renderables(row)
 
 
 class DstackRichHandler(RichHandler):
-    __log_render: Optional[LogRender] = None
+    __log_render = DstackLogRender()
 
-    def render(
-        self,
-        *,
-        record: logging.LogRecord,
-        traceback: Optional[Traceback],
-        message_renderable: "ConsoleRenderable",
-    ) -> "ConsoleRenderable":
-        self._lazy_init_render()
-        path = record.name  # the key difference from RichHandler
+    def emit(self, record: logging.LogRecord) -> None:
+        """Invoked by logging."""
+        message = self.format(record)
+        traceback = None
+        if self.rich_tracebacks and record.exc_info and record.exc_info != (None, None, None):
+            exc_type, exc_value, exc_traceback = record.exc_info
+            assert exc_type is not None
+            assert exc_value is not None
+            traceback = Traceback.from_exception(
+                exc_type,
+                exc_value,
+                exc_traceback,
+                width=self.tracebacks_width,
+                extra_lines=self.tracebacks_extra_lines,
+                theme=self.tracebacks_theme,
+                word_wrap=self.tracebacks_word_wrap,
+                show_locals=self.tracebacks_show_locals,
+                locals_max_length=self.locals_max_length,
+                locals_max_string=self.locals_max_string,
+                suppress=self.tracebacks_suppress,
+            )
+            message = record.getMessage()
+            if self.formatter:
+                record.message = record.getMessage()
+                formatter = self.formatter
+                if hasattr(formatter, "usesTime") and formatter.usesTime():
+                    record.asctime = formatter.formatTime(record, formatter.datefmt)
+                message = formatter.formatMessage(record)
+
+        message_renderable = self.render_message(record, message)
+        path = record.name
         level = self.get_level_text(record)
         time_format = None if self.formatter is None else self.formatter.datefmt
         log_time = datetime.fromtimestamp(record.created)
@@ -112,8 +106,8 @@ class DstackRichHandler(RichHandler):
             line_no=record.lineno,
             link_path=record.pathname if self.enable_link_path else None,
         )
-        return log_renderable
 
-    def _lazy_init_render(self):
-        if self.__log_render is None:
-            self.__log_render = LogRender()
+        try:
+            self.console.print(log_renderable, soft_wrap=True)
+        except Exception:
+            self.handleError(record)
