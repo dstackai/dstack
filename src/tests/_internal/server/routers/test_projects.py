ba0a53eb1acaca425c955f9efb2986b4b47f565d
Victor Skvortsov <vds003@gmail.com>
2024-05-08 11:42:50 +0500

    Fix tests

--- src/tests/_internal/server/routers/test_projects.py
+++ src/tests/_internal/server/routers/test_projects.py
@@ -64,6 +64,7 @@ class TestListProjects:
                             "regions": json.loads(backend.config)["regions"],
                             "vpc_name": None,
                             "vpc_ids": None,
+                            "public_ips": None,
                         },
                     }
                 ],
