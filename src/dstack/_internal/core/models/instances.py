82d7e98b904874f342e393e03c34160cbd32bbb4
Sergey Mezentsev <sergeyme@dstack.ai>
2024-05-16 19:28:58 +0400

    Fix InstanceConfiguration class. The instance_id field is optional.

--- src/dstack/_internal/core/models/instances.py
+++ src/dstack/_internal/core/models/instances.py
@@ -72,7 +72,7 @@ class DockerConfig(CoreModel):
 class InstanceConfiguration(CoreModel):
     project_name: str
     instance_name: str  # unique in pool
-    instance_id: str
+    instance_id: Optional[str]
     ssh_keys: List[SSHKey]
     job_docker_config: Optional[DockerConfig]
     user: str  # dstack user name
