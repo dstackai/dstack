dstackai/dstack

commit 6e4363fc67acb82f8743eeecda456442ff073043

More flexible OCI credentials configuration (#1289)
- In client credentials, replace the key path with
  the key content to allow configuring without any
  file paths
- In default credentials, allow overriding the
  config file path and choosing a profile from the
  config file

This is part of the OCI backend implementation.
Behind the OCI_BACKEND feature flag.

src/dstack/_internal/core/backends/oci/auth.py
@@ -8,7 +8,7 @@ from dstack._internal.core.models.common import is_core_model_instance
 
 def get_client_config(creds: AnyOCICreds) -> Mapping[str, Any]:
     if is_core_model_instance(creds, OCIDefaultCreds):
-        return oci.config.from_file()
+        return oci.config.from_file(file_location=str(creds.file), profile_name=creds.profile)
     return creds.dict(exclude={"type"})
 
 

src/dstack/_internal/core/models/backends/oci.py
@@ -18,8 +18,8 @@ class OCIClientCreds(CoreModel):
     type: Annotated[Literal["client"], Field(description="The type of credentials")] = "client"
     user: Annotated[str, Field(description="User OCID")]
     tenancy: Annotated[str, Field(description="Tenancy OCID")]
-    key_file: Annotated[Path, Field(description="Path to the PEM key")]
-    fingerprint: Annotated[str, Field(description="Key fingerprint")]
+    key_content: Annotated[str, Field(description="Content of the user's private PEM key")]
+    fingerprint: Annotated[str, Field(description="User's public key fingerprint")]
     region: Annotated[
         str, Field(description="Name or key of any region the tenancy is subscribed to")
     ]
@@ -27,6 +27,10 @@ class OCIClientCreds(CoreModel):
 
 class OCIDefaultCreds(CoreModel):
     type: Annotated[Literal["default"], Field(description="The type of credentials")] = "default"
+    file: Annotated[Path, Field(description="Path to the OCI CLI-compatible config file")] = Path(
+        "~/.oci/config"
+    )
+    profile: Annotated[str, Field(description="Profile to load from the config file")] = "DEFAULT"
 
 
 AnyOCICreds = Union[OCIClientCreds, OCIDefaultCreds]

commit ba4f7e263d89bca4301613de64c1470a0f1a3914

More flexible OCI credentials - fixes
This fixes some issues introduced by PR #1289:
- Fix unit tests
- Do not use pathlib.Path in credentials model, as
  it is rendered incorrectly in YAML

src/dstack/_internal/core/backends/oci/auth.py
@@ -8,7 +8,7 @@ from dstack._internal.core.models.common import is_core_model_instance
 
 def get_client_config(creds: AnyOCICreds) -> Mapping[str, Any]:
     if is_core_model_instance(creds, OCIDefaultCreds):
-        return oci.config.from_file(file_location=str(creds.file), profile_name=creds.profile)
+        return oci.config.from_file(file_location=creds.file, profile_name=creds.profile)
     return creds.dict(exclude={"type"})
 
 

src/dstack/_internal/core/models/backends/oci.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from typing import Dict
 
 from pydantic import Field
@@ -27,7 +26,7 @@ class OCIClientCreds(CoreModel):
 
 class OCIDefaultCreds(CoreModel):
     type: Annotated[Literal["default"], Field(description="The type of credentials")] = "default"
-    file: Annotated[Path, Field(description="Path to the OCI CLI-compatible config file")] = Path(
+    file: Annotated[str, Field(description="Path to the OCI CLI-compatible config file")] = (
         "~/.oci/config"
     )
     profile: Annotated[str, Field(description="Profile to load from the config file")] = "DEFAULT"

src/tests/_internal/server/routers/test_backends.py
@@ -30,7 +30,12 @@ FAKE_OCI_CLIENT_CREDS = {
     "type": "client",
     "user": "ocid1.user.oc1..aaaaaaaa",
     "tenancy": "ocid1.tenancy.oc1..aaaaaaaa",
-    "key_file": "/dev/null",
+    "key_content": (
+        "-----BEGIN PRIVATE KEY-----\n"
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n"
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n"
+        "-----END PRIVATE KEY-----"
+    ),
     "fingerprint": "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00",
     "region": "me-dubai-1",
 }