781fa9be3f17168d7d6564d99abd9a42516f08dd
Victor Skvortsov <vds003@gmail.com>
2024-06-14 11:29:11 +0500

    Update gpuhunt to 0.0.10

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ BASE_DEPS = [
     "cachetools",
     "dnspython",
     "grpcio>=1.50",  # indirect
-    "gpuhunt>=0.0.9",
+    "gpuhunt>=0.0.10",
     "sentry-sdk[fastapi]",
     "httpx",
     "aiorwlock",
