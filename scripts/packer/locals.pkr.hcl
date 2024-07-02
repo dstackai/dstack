adf7c8d46e37718044fc25b9b5165f61827329fe
Jvst Me <git@jvst.me>
2024-07-02 07:55:24 +0200

    Add nvidia-fabricmanager to dstack VM images

--- scripts/packer/locals.pkr.hcl
+++ scripts/packer/locals.pkr.hcl
@@ -1,6 +1,6 @@
 locals {
   clean_image_version = regex_replace(var.image_version, "[^a-z0-9-]", "-")
   image_name = "${var.build_prefix}dstack-${local.clean_image_version}"
-  docker_version = "20.10.17"
-  cuda_drivers_version = "535.54.03-1"
+  docker_version = ""
+  cuda_drivers_version = ""
 }
--- scripts/packer/provisioners/install-nvidia-grid-driver-for-azure.sh
+++ scripts/packer/provisioners/install-nvidia-grid-driver-for-azure.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential linux-azure -y
 
 wget --no-verbose -O NVIDIA-Linux-x86_64-grid.run \
-    https://download.microsoft.com/download/1/4/4/14450d0e-a3f2-4b0a-9bb4-a8e729e986c4/NVIDIA-Linux-x86_64-535.154.05-grid-azure.run
+    https://download.microsoft.com/download/8/d/a/8da4fb8e-3a9b-4e6a-bc9a-72ff64d7a13c/NVIDIA-Linux-x86_64-535.161.08-grid-azure.run
 chmod +x NVIDIA-Linux-x86_64-grid.run
 sudo ./NVIDIA-Linux-x86_64-grid.run --silent --disable-nouveau
 rm NVIDIA-Linux-x86_64-grid.run
