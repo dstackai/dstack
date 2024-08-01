b61dceb5168dc633273c72f3140eafaa560834fc
Victor Skvortsov <vds003@gmail.com>
2024-08-01 10:17:55 +0500

    Fix instances not added to existing fleets (#1486)

--- src/dstack/_internal/server/db.py
+++ src/dstack/_internal/server/db.py
@@ -28,7 +28,7 @@ class Database:
                 cursor = dbapi_connection.cursor()
                 cursor.execute("PRAGMA journal_mode=WAL;")
                 cursor.execute("PRAGMA foreign_keys=ON;")
-                cursor.execute("PRAGMA busy_timeout=5000;")
+                cursor.execute("PRAGMA busy_timeout=10000;")
                 cursor.close()
 
     def get_dialect_name(self) -> str:
