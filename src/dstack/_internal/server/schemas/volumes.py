9e726aab135a137f08d63a5243a2a0d1adbe3a60
Victor Skvortsov <vds003@gmail.com>
2024-08-07 10:36:52 +0500

    Implement API endpoint for listing volumes across projects

--- src/dstack/_internal/server/schemas/volumes.py
+++ src/dstack/_internal/server/schemas/volumes.py
@@ -1,7 +1,20 @@
-from typing import List
+from datetime import datetime
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import Field
 
 from dstack._internal.core.models.common import CoreModel
 from dstack._internal.core.models.volumes import VolumeConfiguration
+
+
+class ListVolumesRequest(CoreModel):
+    project_name: Optional[str]
+    only_active: bool = False
+    prev_created_at: Optional[datetime]
+    prev_id: Optional[UUID]
+    limit: int = Field(100, ge=0, le=100)
+    ascending: bool = False
 
 
 class GetVolumeRequest(CoreModel):
