fd8cdf1fc4e6bc1ff9038ae8385d2ef119c04b88
swsvc <23121066+swsvc@users.noreply.github.com>
2024-08-30 12:34:24 +0300

    Add Support for ECDSA and Ed25519 keys (#1641)
    Closes: https://github.com/dstackai/dstack/issues/1443

--- src/dstack/_internal/server/background/tasks/process_instances.py
+++ src/dstack/_internal/server/background/tasks/process_instances.py
@@ -6,7 +6,7 @@ from uuid import UUID
 
 import requests
 from paramiko.pkey import PKey
-from paramiko.ssh_exception import PasswordRequiredException, SSHException
+from paramiko.ssh_exception import PasswordRequiredException
 from pydantic import ValidationError
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
@@ -72,7 +72,7 @@ from dstack._internal.utils.common import get_current_datetime
 from dstack._internal.utils.logging import get_logger
 from dstack._internal.utils.network import get_ip_from_network
 from dstack._internal.utils.ssh import (
-    rsa_pkey_from_str,
+    pkey_from_str,
 )
 
 PENDING_JOB_RETRY_INTERVAL = timedelta(seconds=60)
@@ -249,7 +249,7 @@ async def add_remote(instance_id: UUID) -> None:
             # Prepare connection key
             try:
                 pkeys = [
-                    rsa_pkey_from_str(sk.private)
+                    pkey_from_str(sk.private)
                     for sk in remote_details.ssh_keys
                     if sk.private is not None
                 ]
@@ -271,14 +271,14 @@ async def add_remote(instance_id: UUID) -> None:
                     },
                 )
                 return
-            except SSHException:
+            except ValueError:
                 instance.status = InstanceStatus.TERMINATED
                 instance.deleted = True
                 instance.deleted_at = get_current_datetime()
-                instance.termination_reason = "Cannot parse private key, RSA key required"
+                instance.termination_reason = "Cannot parse private key, key type is not supported"
                 await session.commit()
                 logger.warning(
-                    "Failed to start instance %s: private SSH key is not a valid RSA key",
+                    "Failed to start instance %s: unsupported private SSH key type",
                     instance.name,
                     extra={
                         "instance_name": instance.name,
