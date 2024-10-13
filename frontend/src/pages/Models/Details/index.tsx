ee263e8ac935e85d051381bbb02995881e1c0802
Oleg <vavilovolegik@gmail.com>
2024-10-13 22:58:05 +0300

    Implement View Code on the Model page in the UI and small improvements (#1825)
    * [UX]: Send by Enter in the model chat #1552
    
    * [UX]: Send by Enter in the model chat #1552
    
    * [UI] Add a Code button to the model playground page (similar to OpenAI/Anthropic) #1639

--- frontend/src/pages/Models/Details/index.tsx
+++ frontend/src/pages/Models/Details/index.tsx
@@ -3,9 +3,7 @@ import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import reactStringReplace from 'react-string-replace';
-import { get as _get } from 'lodash';
 import cn from 'classnames';
-import { format } from 'date-fns';
 import OpenAI from 'openai';
 
 import {
@@ -20,22 +18,26 @@ import {
     Header,
     ListEmptyMessage,
     Loader,
+    Modal,
     NavigateLink,
-    StatusIndicator,
+    SelectCSD,
+    SelectCSDProps,
+    SpaceBetween,
 } from 'components';
 
 import { useAppSelector, useBreadcrumbs, useNotifications } from 'hooks';
-import { getExtendedModelFromRun, getStatusIconType } from 'libs/run';
+import { getExtendedModelFromRun } from 'libs/run';
 import { ROUTES } from 'routes';
 import { useGetRunQuery } from 'services/run';
 
 import { selectAuthToken } from 'App/slice';
 
-import { DATE_TIME_FORMAT } from '../../../consts';
+import { copyToClipboard } from '../../../libs';
 
 import { IModelExtended } from '../List/types';
 import { FormValues, Message, Role } from './types';
 
+import { ReactComponent as SourceIcon } from 'assets/icons/source.svg';
 import css from './styles.module.scss';
 
 const MESSAGE_ROLE_MAP: Record<Role, string> = {
@@ -45,15 +47,23 @@ const MESSAGE_ROLE_MAP: Record<Role, string> = {
     assistant: 'Assistant',
 };
 
+const VIEW_CODE_TYPE_OPTIONS = [
+    { label: 'Python', value: 'python' },
+    { label: 'Curl', value: 'curl' },
+];
+
 export const ModelDetails: React.FC = () => {
     const { t } = useTranslation();
     const token = useAppSelector(selectAuthToken);
     const [messages, setMessages] = useState<Message[]>([]);
+    const [viewCodeVisible, setViewCodeVisible] = useState<boolean>(false);
+    const [selectedCode, setSelectedCode] = useState<SelectCSDProps.Option>(VIEW_CODE_TYPE_OPTIONS[0]);
     const [loading, setLoading] = useState<boolean>(false);
     const params = useParams();
     const paramProjectName = params.projectName ?? '';
     const paramRunName = params.runName ?? '';
     const openai = useRef<OpenAI>();
+    const textAreaRef = useRef<HTMLDivElement>(null);
     const chatList = useRef<HTMLElement>();
     const [pushNotification] = useNotifications();
 
@@ -114,7 +124,9 @@ export const ModelDetails: React.FC = () => {
         scrollChatToBottom();
     }, [messageForShowing]);
 
-    const { handleSubmit, control, setValue } = useForm<FormValues>();
+    const { handleSubmit, control, setValue, watch } = useForm<FormValues>();
+
+    const messageText = watch('message');
 
     const sendRequest = async (messages: Message[]) => {
         if (!openai.current) return Promise.reject('Model not found');
@@ -208,7 +220,7 @@ export const ModelDetails: React.FC = () => {
             [...matches].forEach((l) => l && languages.push(l.replace(/^```/, '')));
         }
 
-        const replacedStrings = reactStringReplace(content, PATTERN, (match, i) => {
+        const replacedStrings = reactStringReplace(content, PATTERN, (match) => {
             if (!match) {
                 return '';
             }
@@ -233,6 +245,62 @@ export const ModelDetails: React.FC = () => {
         );
     };
 
+    const onChangeMessage = () => {
+        if (!textAreaRef.current) return;
+
+        const textAreaElement = textAreaRef.current.querySelector('textarea');
+
+        if (!textAreaElement) return;
+
+        textAreaElement.style.height = 'auto';
+        textAreaElement.style.height = textAreaElement.scrollHeight + 'px';
+    };
+
+    const onKeyDown = (event) => {
+        if (event?.detail?.keyCode === 13 && !event?.detail?.ctrlKey) {
+            handleSubmit(onSubmit)();
+        } else if (event?.detail?.keyCode === 13 && event?.detail?.ctrlKey) {
+            event.preventDefault();
+            setValue('message', messageText + '\n');
+            setTimeout(onChangeMessage, 0);
+        }
+    };
+
+    const pythonCode = `from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="${modelData?.name ?? ''}",
+  messages=[],
+  stream=True,
+  max_tokens=512,
+  response_format={
+    "type": "text"
+  }
+)`;
+
+    const curlCode = `curl https://api.openai.com/v1/chat/completions \\
+  -H "Content-Type: application/json" \\
+  -H "Authorization: Bearer $OPENAI_API_KEY" \\
+  -d '{
+  "model": "${modelData?.name ?? ''}",
+  "messages": [],
+  "stream": true,
+  "max_tokens": 512,
+  "response_format": {
+    "type": "text"
+  }
+}'`;
+
+    const onCopyCode = () => {
+        switch (selectedCode.value) {
+            case VIEW_CODE_TYPE_OPTIONS[0].value:
+                return copyToClipboard(pythonCode);
+            case VIEW_CODE_TYPE_OPTIONS[1].value:
+                return copyToClipboard(curlCode);
+        }
+    };
+
     return (
         <ContentLayout
             header={
@@ -252,76 +320,119 @@ export const ModelDetails: React.FC = () => {
             )}
 
             {modelData && (
-                <div className={css.modelDetailsLayout}>
-                    <div className={css.general}>
-                        <Container header={<Header variant="h2">{t('common.general')}</Header>}>
-                            <ColumnLayout columns={4} variant="text-grid">
-                                <div>
-                                    <Box variant="awsui-key-label">{t('models.details.run_name')}</Box>
-                                    <div>{modelData.run_name}</div>
-                                </div>
+                <>
+                    <div className={css.modelDetailsLayout}>
+                        <div className={css.general}>
+                            <Container header={<Header variant="h2">{t('common.general')}</Header>}>
+                                <ColumnLayout columns={4} variant="text-grid">
+                                    <div>
+                                        <Box variant="awsui-key-label">{t('models.details.run_name')}</Box>
+                                        <div>{modelData.run_name}</div>
+                                    </div>
 
-                                <div>
-                                    <Box variant="awsui-key-label">{t('models.model_name')}</Box>
-                                    <div>{modelData.name}</div>
-                                </div>
+                                    <div>
+                                        <Box variant="awsui-key-label">{t('models.model_name')}</Box>
+                                        <div>{modelData.name}</div>
+                                    </div>
 
-                                <div>
-                                    <Box variant="awsui-key-label">{t('models.type')}</Box>
-                                    <div>{modelData.type}</div>
+                                    <div>
+                                        <Box variant="awsui-key-label">{t('models.type')}</Box>
+                                        <div>{modelData.type}</div>
+                                    </div>
+                                </ColumnLayout>
+                            </Container>
+                        </div>
+
+                        <form className={css.modelForm} onSubmit={handleSubmit(onSubmit)}>
+                            <div className={css.buttons}>
+                                <Button iconSvg={<SourceIcon />} disabled={loading} onClick={() => setViewCodeVisible(true)} />
+
+                                <Button iconName="remove" disabled={loading || !messages.length} onClick={clearChat} />
+                            </div>
+
+                            <aside className={css.side}>
+                                <FormTextarea
+                                    disabled={loading}
+                                    label={t('models.details.instructions')}
+                                    constraintText={t('models.details.instructions_description')}
+                                    control={control}
+                                    name="instructions"
+                                />
+                            </aside>
+
+                            <div className={css.chat} ref={chatList}>
+                                {!messageForShowing.length && (
+                                    <ListEmptyMessage
+                                        title={t('models.details.chat_empty_title')}
+                                        message={t('models.details.chat_empty_message')}
+                                    />
+                                )}
+
+                                {messageForShowing.map((message, index) => (
+                                    <div key={index} className={cn(css.message, css[message.role])}>
+                                        <Box variant="h4">
+                                            {MESSAGE_ROLE_MAP[message.role as keyof typeof MESSAGE_ROLE_MAP]}
+                                        </Box>
+                                        {renderMessageBody(message.content || '...')}
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div ref={textAreaRef} className={css.messageForm}>
+                                <FormTextarea
+                                    stretch
+                                    placeholder={t('models.details.message_placeholder')}
+                                    control={control}
+                                    disabled={loading}
+                                    name="message"
+                                    onKeyDown={onKeyDown}
+                                    onChange={onChangeMessage}
+                                />
+
+                                <div className={css.buttons}>
+                                    <Button variant="primary" disabled={loading}>
+                                        {t('common.send')}
+                                    </Button>
                                 </div>
-                            </ColumnLayout>
-                        </Container>
+                            </div>
+                        </form>
                     </div>
 
-                    <form className={css.modelForm} onSubmit={handleSubmit(onSubmit)}>
-                        <div className={css.clear}>
-                            <Button iconName="remove" disabled={loading || !messages.length} onClick={clearChat} />
-                        </div>
-
-                        <aside className={css.side}>
-                            <FormTextarea
-                                disabled={loading}
-                                label={t('models.details.instructions')}
-                                constraintText={t('models.details.instructions_description')}
-                                control={control}
-                                name="instructions"
-                            />
-                        </aside>
-
-                        <div className={css.chat} ref={chatList}>
-                            {!messageForShowing.length && (
-                                <ListEmptyMessage
-                                    title={t('models.details.chat_empty_title')}
-                                    message={t('models.details.chat_empty_message')}
-                                />
-                            )}
-
-                            {messageForShowing.map((message, index) => (
-                                <div key={index} className={cn(css.message, css[message.role])}>
-                                    <Box variant="h4">{MESSAGE_ROLE_MAP[message.role as keyof typeof MESSAGE_ROLE_MAP]}</Box>
-                                    {renderMessageBody(message.content || '...')}
-                                </div>
-                            ))}
-                        </div>
-
-                        <div className={css.messageForm}>
-                            <FormTextarea
-                                stretch
-                                placeholder={t('models.details.message_placeholder')}
-                                control={control}
-                                disabled={loading}
-                                name="message"
-                            />
-
-                            <div className={css.buttons}>
-                                <Button variant="primary" disabled={loading}>
-                                    {t('common.send')}
+                    <Modal
+                        visible={viewCodeVisible}
+                        header={t('models.details.view_code')}
+                        size="large"
+                        footer={
+                            <Box float="right">
+                                <Button variant="normal" onClick={() => setViewCodeVisible(false)}>
+                                    {t('common.close')}
                                 </Button>
+                            </Box>
+                        }
+                        onDismiss={() => setViewCodeVisible(false)}
+                    >
+                        <SpaceBetween size="m" direction="vertical">
+                            <Box>{t('models.details.view_code_description')}</Box>
+
+                            <div className={css.viewCodeControls}>
+                                <SelectCSD
+                                    options={VIEW_CODE_TYPE_OPTIONS}
+                                    selectedOption={selectedCode}
+                                    expandToViewport={true}
+                                    onChange={(event) => {
+                                        setSelectedCode(event.detail.selectedOption);
+                                    }}
+                                />
+
+                                <Button iconName="copy" onClick={onCopyCode}></Button>
                             </div>
-                        </div>
-                    </form>
-                </div>
+
+                            {selectedCode.value === VIEW_CODE_TYPE_OPTIONS[0].value && <Code>{pythonCode}</Code>}
+
+                            {selectedCode.value === VIEW_CODE_TYPE_OPTIONS[1].value && <Code>{curlCode}</Code>}
+                        </SpaceBetween>
+                    </Modal>
+                </>
             )}
         </ContentLayout>
     );
