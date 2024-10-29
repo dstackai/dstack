4330c3f560b12659f0ec79a7b2bebe2a5a4f8735
Stanislav Zmiev <zmievsa@gmail.com>
2024-10-29 09:39:57 +0100

    Update pydantic-duality (#1902)

--- setup.py
+++ setup.py
@@ -46,8 +46,7 @@ BASE_DEPS = [
     "starlette>=0.26.0",
     "uvicorn",
     "pydantic>=1.10.10,<2.0.0",
-    # https://github.com/zmievsa/pydantic-duality/issues/21
-    "pydantic-duality<=1.2.2",
+    "pydantic-duality>=1.2.4",
     "sqlalchemy[asyncio]>=2.0.0",
     "sqlalchemy_utils>=0.40.0",
     "alembic>=1.10.2",
