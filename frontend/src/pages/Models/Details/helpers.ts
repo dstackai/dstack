69eb0cfa94ae6123bf65ddadd143dea941d49a8b
Oleg <vavilovolegik@gmail.com>
2024-11-21 22:45:13 +0300

    [UI] Model page code snippets fixes and improvements #2024 (#2026)

--- frontend/src/pages/Models/Details/helpers.ts
+++ frontend/src/pages/Models/Details/helpers.ts
@@ -5,32 +5,36 @@ import { IModelExtended } from '../List/types';
 export const getPythonModelCode = ({ model, token }: { model?: IModelExtended | null; token?: string }) => {
     return `from openai import OpenAI
 client = OpenAI(
-    api_key="${token}"
+    api_key="${token}",
     base_url="${getModelGateway(model?.base_url ?? '')}"
 )
 
 response = client.chat.completions.create(
-  model="${model?.name ?? ''}",
-  messages=[],
-  stream=True,
-  max_tokens=512,
-  response_format={
-    "type": "text"
-  }
+    model="${model?.name ?? ''}",
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello world",
+        },
+    ],
+    stream=True,
+    max_tokens=512,
 )`;
 };
 
 export const getCurlModelCode = ({ model, token }: { model?: IModelExtended | null; token?: string }) => {
-    return `curl ${getModelGateway(model?.base_url ?? '')} \\
+    return `curl ${getModelGateway(model?.base_url ?? '') + 'chat/completions'} \\
   -H "Content-Type: application/json" \\
   -H "Authorization: Bearer ${token}" \\
   -d '{
   "model": "${model?.name ?? ''}",
-  "messages": [],
+  "messages": [
+    {
+      "role": "user",
+      "content": "Hello world"
+    }
+  ],
   "stream": true,
   "max_tokens": 512,
-  "response_format": {
-    "type": "text"
-  }
 }'`;
 };
