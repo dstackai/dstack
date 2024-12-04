bb30988237bf54c8b41b6d36ed2889bb841d6764
Victor Skvortsov <vds003@gmail.com>
2024-12-04 11:04:33 +0500

    Handle client backward compatibility

--- src/dstack/api/server/_fleets.py
+++ src/dstack/api/server/_fleets.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import parse_obj_as
 
@@ -29,11 +29,7 @@ class FleetsAPIClient(APIClientGroup):
         spec: FleetSpec,
     ) -> FleetPlan:
         body = GetFleetPlanRequest(spec=spec)
-        body_json = body.json()
-        if spec.configuration_path is None:
-            # Handle old server versions that do not accept configuration_path
-            # TODO: Can be removed in 0.19
-            body_json = body.json(exclude={"spec": {"configuration_path"}})
+        body_json = body.json(exclude=_get_fleet_spec_excludes(spec))
         resp = self._request(f"/api/project/{project_name}/fleets/get_plan", body=body_json)
         return parse_obj_as(FleetPlan.__response__, resp.json())
 
@@ -43,11 +39,7 @@ class FleetsAPIClient(APIClientGroup):
         spec: FleetSpec,
     ) -> Fleet:
         body = CreateFleetRequest(spec=spec)
-        body_json = body.json()
-        if spec.configuration_path is None:
-            # Handle old server versions that do not accept configuration_path
-            # TODO: Can be removed in 0.19
-            body_json = body.json(exclude={"spec": {"configuration_path"}})
+        body_json = body.json(exclude=_get_fleet_spec_excludes(spec))
         resp = self._request(f"/api/project/{project_name}/fleets/create", body=body_json)
         return parse_obj_as(Fleet.__response__, resp.json())
 
@@ -58,3 +50,19 @@ class FleetsAPIClient(APIClientGroup):
     def delete_instances(self, project_name: str, name: str, instance_nums: List[int]) -> None:
         body = DeleteFleetInstancesRequest(name=name, instance_nums=instance_nums)
         self._request(f"/api/project/{project_name}/fleets/delete_instances", body=body.json())
+
+
+def _get_fleet_spec_excludes(fleet_spec: FleetSpec) -> Optional[dict]:
+    exclude = {}
+    # TODO: Can be removed in 0.19
+    if fleet_spec.configuration_path is None:
+        exclude["spec"] = {"configuration_path"}
+    if fleet_spec.configuration.ssh_config is not None:
+        if all(
+            isinstance(h, str) or h.internal_ip is None
+            for h in fleet_spec.configuration.ssh_config.hosts
+        ):
+            exclude["spec"] = {
+                "configuration": {"ssh_config": {"hosts": {"__all__": {"internal_ip"}}}}
+            }
+    return exclude or None
