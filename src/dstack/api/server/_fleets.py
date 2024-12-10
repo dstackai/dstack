8577df17e9c143bb0aa68c904453db8148d1f08e
Dmitry Meyer <me@undef.im>
2024-12-10 14:09:56 +0000

    Fix `reservation` property backward compatibility
    Fixes: https://github.com/dstackai/dstack/issues/2076

--- src/dstack/api/server/_fleets.py
+++ src/dstack/api/server/_fleets.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import parse_obj_as
 
@@ -52,25 +52,32 @@ class FleetsAPIClient(APIClientGroup):
         self._request(f"/api/project/{project_name}/fleets/delete_instances", body=body.json())
 
 
-def _get_fleet_spec_excludes(fleet_spec: FleetSpec) -> Optional[dict]:
-    exclude = {}
+_ExcludeDict = dict[str, Union[bool, set[str], "_ExcludeDict"]]
+
+
+def _get_fleet_spec_excludes(fleet_spec: FleetSpec) -> Optional[_ExcludeDict]:
+    spec_excludes: _ExcludeDict = {}
+    configuration_excludes: _ExcludeDict = {}
+    profile_excludes: set[str] = set()
+
     # TODO: Can be removed in 0.19
     if fleet_spec.configuration_path is None:
-        exclude["spec"] = {"configuration_path"}
+        spec_excludes["configuration_path"] = True
     if fleet_spec.configuration.ssh_config is not None:
         if all(
             isinstance(h, str) or h.internal_ip is None
             for h in fleet_spec.configuration.ssh_config.hosts
         ):
-            exclude["spec"] = {
-                "configuration": {"ssh_config": {"hosts": {"__all__": {"internal_ip"}}}}
-            }
-    # client >= 0.18.26 / server <= 0.18.25 compatibility tweak
+            configuration_excludes["ssh_config"] = {"hosts": {"__all__": {"internal_ip"}}}
+    # client >= 0.18.30 / server <= 0.18.29 compatibility tweak
     if not fleet_spec.configuration.reservation:
-        exclude.setdefault("spec", {})
-        exclude["spec"].setdefault("configuration", set())
-        exclude["spec"]["configuration"].add("reservation")
-        exclude["spec"].setdefault("profile", set())
-        exclude["spec"]["profile"].add("reservation")
+        configuration_excludes["reservation"] = True
+        profile_excludes.add("reservation")
 
-    return exclude or None
+    if configuration_excludes:
+        spec_excludes["configuration"] = configuration_excludes
+    if profile_excludes:
+        spec_excludes["profile"] = profile_excludes
+    if spec_excludes:
+        return {"spec": spec_excludes}
+    return None
--- src/dstack/api/server/_runs.py
+++ src/dstack/api/server/_runs.py
@@ -114,22 +114,29 @@ class RunsAPIClient(APIClientGroup):
 
 
 def _get_run_spec_excludes(run_spec: RunSpec) -> Optional[dict]:
+    spec_excludes: dict[str, set[str]] = {}
     configuration_excludes: set[str] = set()
     profile_excludes: set[str] = set()
     configuration = run_spec.configuration
+
     # client >= 0.18.18 / server <= 0.18.17 compatibility tweak
     if not configuration.privileged:
         configuration_excludes.add("privileged")
-    # client >= 0.18.29 / server <= 0.18.28 compatibility tweak
-    if not configuration.reservation:
-        configuration_excludes.add("reservation")
-        profile_excludes.add("reservation")
     # client >= 0.18.23 / server <= 0.18.22 compatibility tweak
     if configuration.type == "service" and configuration.gateway is None:
         configuration_excludes.add("gateway")
     # client >= 0.18.30 / server <= 0.18.29 compatibility tweak
     if run_spec.configuration.user is None:
         configuration_excludes.add("user")
+    # client >= 0.18.30 / server <= 0.18.29 compatibility tweak
+    if not configuration.reservation:
+        configuration_excludes.add("reservation")
+        profile_excludes.add("reservation")
+
     if configuration_excludes:
-        return {"run_spec": {"configuration": configuration_excludes, "profile": profile_excludes}}
+        spec_excludes["configuration"] = configuration_excludes
+    if profile_excludes:
+        spec_excludes["profile"] = profile_excludes
+    if spec_excludes:
+        return {"run_spec": spec_excludes}
     return None
