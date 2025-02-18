ff6c77c90a22cd6c0fd22d618cf9d444993c9380
Jvst Me <git@jvst.me>
2025-02-18 21:17:26 +0100

    [UI]: Fix job details for multi-job runs

--- frontend/src/pages/Runs/Details/Jobs/List/helpers.ts
+++ frontend/src/pages/Runs/Details/Jobs/List/helpers.ts
@@ -3,41 +3,41 @@ import { format } from 'date-fns';
 import { DATE_TIME_FORMAT } from 'consts';
 
 export const getJobListItemResources = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.instance_type?.resources?.description;
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.instance_type?.resources?.description;
 };
 
 export const getJobListItemSpot = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.instance_type?.resources?.spot?.toString() ?? '-';
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.instance_type?.resources?.spot?.toString() ?? '-';
 };
 
 export const getJobListItemPrice = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.price
-        ? `$${job.job_submissions?.[0]?.job_provisioning_data?.price}`
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.price
+        ? `$${job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.price}`
         : null;
 };
 
 export const getJobListItemInstance = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.instance_type?.name;
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.instance_type?.name;
 };
 
 export const getJobListItemRegion = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.region ?? '-';
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.region ?? '-';
 };
 
 export const getJobListItemBackend = (job: IJob) => {
-    return job.job_submissions?.[0]?.job_provisioning_data?.backend ?? '-';
+    return job.job_submissions?.[job.job_submissions.length - 1]?.job_provisioning_data?.backend ?? '-';
 };
 
 export const getJobSubmittedAt = (job: IJob) => {
-    return job.job_submissions?.[0].submitted_at
-        ? format(new Date(job.job_submissions?.[0].submitted_at), DATE_TIME_FORMAT)
+    return job.job_submissions?.[job.job_submissions.length - 1].submitted_at
+        ? format(new Date(job.job_submissions?.[job.job_submissions.length - 1].submitted_at), DATE_TIME_FORMAT)
         : '';
 };
 
 export const getJobStatus = (job: IJob) => {
-    return job.job_submissions?.[0].status;
+    return job.job_submissions?.[job.job_submissions.length - 1].status;
 };
 
 export const getJobTerminationReason = (job: IJob) => {
-    return job.job_submissions?.[0].termination_reason ?? '-';
+    return job.job_submissions?.[job.job_submissions.length - 1].termination_reason ?? '-';
 };
