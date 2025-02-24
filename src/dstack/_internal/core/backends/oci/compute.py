d4305ae082df185dd832bf3fd436b05bd2469ed7
Victor Skvortsov <vds003@gmail.com>
2025-02-24 12:12:49 +0500

    Use unique resource names on OCI

--- src/dstack/_internal/core/backends/oci/compute.py
+++ src/dstack/_internal/core/backends/oci/compute.py
@@ -6,6 +6,7 @@ import oci
 
 from dstack._internal.core.backends.base.compute import (
     Compute,
+    generate_unique_instance_name,
     get_job_instance_name,
     get_user_data,
 )
@@ -152,6 +153,7 @@ class OCICompute(Compute):
         ]
         cloud_init_user_data = get_user_data(instance_config.get_public_keys(), setup_commands)
 
+        display_name = generate_unique_instance_name(instance_config)
         try:
             instance = resources.launch_instance(
                 region=region,
@@ -159,7 +161,7 @@ class OCICompute(Compute):
                 compartment_id=self.config.compartment_id,
                 subnet_id=subnet.id,
                 security_group_id=security_group.id,
-                display_name=instance_config.instance_name,
+                display_name=display_name,
                 cloud_init_user_data=cloud_init_user_data,
                 shape=instance_offer.instance.name,
                 is_spot=instance_offer.instance.resources.spot,
