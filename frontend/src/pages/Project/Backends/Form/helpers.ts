6bea818a95ad5cc74b6f87d10fd0fa78dfd34a3a
Oleg <vavilovolegik@gmail.com>
2025-03-17 22:01:15 +0300

    Simplify implementation of new backends #2372 (#2423)

--- frontend/src/pages/Project/Backends/Form/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const prepareBackendConfigForApi = (backendConfig: TBackendConfig): TBackendConfig => {
-    if (backendConfig.type !== 'aws' || !backendConfig.vpc_name) return backendConfig;
-
-    return {
-        ...backendConfig,
-        vpc_name: backendConfig.vpc_name.trim(),
-    };
-};
