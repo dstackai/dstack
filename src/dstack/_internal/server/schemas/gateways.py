bddd28d953736bcd5e980032d5cdc9e17584008f
Victor Skvortsov <vds003@gmail.com>
2025-03-17 11:33:13 +0500

    Restore removed models fields for compatibility with 0.18 clients (#2414)

--- src/dstack/_internal/server/schemas/gateways.py
+++ src/dstack/_internal/server/schemas/gateways.py
@@ -1,11 +1,25 @@
-from typing import List
+from typing import Annotated, Any, Dict, List, Optional
 
+from pydantic import Field
+
+from dstack._internal.core.models.backends.base import BackendType
 from dstack._internal.core.models.common import CoreModel
 from dstack._internal.core.models.gateways import GatewayConfiguration
 
 
 class CreateGatewayRequest(CoreModel):
     configuration: GatewayConfiguration
+    # Deprecated and unused. Left for compatibility with 0.18 clients.
+    name: Annotated[Optional[str], Field(exclude=True)] = None
+    backend_type: Annotated[Optional[BackendType], Field(exclude=True)] = None
+    region: Annotated[Optional[str], Field(exclude=True)] = None
+
+    class Config:
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any]) -> None:
+            del schema["properties"]["name"]
+            del schema["properties"]["backend_type"]
+            del schema["properties"]["region"]
 
 
 class GetGatewayRequest(CoreModel):
