f7117ad0258c119029f4ae4e900bcd859521cbbb
Victor Skvortsov <vds003@gmail.com>
2025-03-18 11:50:27 +0500

    Support multiple domains with Entra login (#2424)

--- frontend/src/App/helpers.ts
+++ frontend/src/App/helpers.ts
@@ -1,3 +1,8 @@
 import { Mode } from '@cloudscape-design/global-styles';
 
 export const getThemeMode = (): Mode => (window?.matchMedia('(prefers-color-scheme: dark)').matches ? Mode.Dark : Mode.Light);
+
+export function getBaseUrl(): string {
+    const { protocol, hostname, port } = window.location;
+    return `${protocol}//${hostname}${port ? `:${port}` : ''}`;
+}
