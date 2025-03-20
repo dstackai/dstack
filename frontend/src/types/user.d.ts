9ba84fe9692ff719c04b3d3d45543a56863ba4f9
Oleg <vavilovolegik@gmail.com>
2025-03-20 08:07:44 +0300

    [Feature]: Support activating/deactivating users via the UI #2427 (#2440)

--- frontend/src/types/user.d.ts
+++ frontend/src/types/user.d.ts
@@ -1,35 +1,36 @@
 declare type TUserRole = 'user' | 'admin';
-declare type TUserPermission = 'CAN_CREATE_PROJECTS'
-declare type TUserPermissionKeys = 'can_create_projects'
+declare type TUserPermission = 'CAN_CREATE_PROJECTS';
+declare type TUserPermissionKeys = 'can_create_projects';
 
 declare interface IUserResponseData {
-    id: string,
+    id: string;
     username: string;
-    global_role: TUserRole
-    email: string | null,
-    permissions: Record<TUserPermissionKeys, boolean>
+    global_role: TUserRole;
+    email: string | null;
+    permissions: Record<TUserPermissionKeys, boolean>;
 }
 
 declare interface IUser {
-    id: string,
+    id: string;
     username: string;
-    global_role: TUserRole
-    email: string | null,
-    permissions: TUserPermission[],
-    created_at: string
+    global_role: TUserRole;
+    email: string | null;
+    permissions: TUserPermission[];
+    created_at: string;
+    active: boolean;
 }
 
 declare interface IUserWithCreds extends IUser {
-    "creds": {
-        "token": string
-    }
+    creds: {
+        token: string;
+    };
 }
 
-declare interface IUserAuthData extends Pick<IUserWithCreds['creds'], 'token'>{}
+declare interface IUserAuthData extends Pick<IUserWithCreds['creds'], 'token'> {}
 
 declare interface IUserBillingInfo {
-    balance: number
-    is_payment_method_attached: boolean,
-    default_payment_amount: number,
-    billing_history: IPayment[]
+    balance: number;
+    is_payment_method_attached: boolean;
+    default_payment_amount: number;
+    billing_history: IPayment[];
 }
