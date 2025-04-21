557ed1bfc970c885e9be1b03bf62b10cb6e246c6
Victor Skvortsov <vds003@gmail.com>
2025-04-21 11:13:50 +0500

    Add try_advisory_lock_ctx

--- src/dstack/_internal/server/services/locking.py
+++ src/dstack/_internal/server/services/locking.py
@@ -2,7 +2,7 @@ import asyncio
 import hashlib
 from asyncio import Lock
 from contextlib import asynccontextmanager
-from typing import Dict, List, Set, Tuple, TypeVar, Union
+from typing import AsyncGenerator, Dict, List, Set, Tuple, TypeVar, Union
 
 from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
@@ -47,6 +47,19 @@ async def advisory_lock_ctx(
         await bind.execute(select(func.pg_advisory_lock(string_to_lock_id(resource))))
     try:
         yield
+    finally:
+        if dialect_name == "postgresql":
+            await bind.execute(select(func.pg_advisory_unlock(string_to_lock_id(resource))))
+
+
+@asynccontextmanager
+async def try_advisory_lock_ctx(
+    bind: Union[AsyncConnection, AsyncSession], dialect_name: str, resource: str
+) -> AsyncGenerator[bool, None]:
+    if dialect_name == "postgresql":
+        res = await bind.execute(select(func.pg_try_advisory_lock(string_to_lock_id(resource))))
+    try:
+        yield res.scalar_one()
     finally:
         if dialect_name == "postgresql":
             await bind.execute(select(func.pg_advisory_unlock(string_to_lock_id(resource))))
