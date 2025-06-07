9ce5de9bcbdbcbc10c558d494e18f09cb9157aa5
Oleg Vavilov <vavilovolegik@gmail.com>
2025-06-07 22:26:17 +0300

    [Bug]: Tab Billing changes to Settings after top up balance https://github.com/dstackai/dstack-cloud/issues/297

--- frontend/src/pages/User/Details/index.tsx
+++ frontend/src/pages/User/Details/index.tsx
@@ -64,15 +64,15 @@ export const UserDetails: React.FC = () => {
             id: UserDetailsTabTypeEnum.SETTINGS,
             href: ROUTES.USER.DETAILS.FORMAT(paramUserName),
         },
-        process.env.UI_VERSION === 'sky' && {
-            label: t('billing.title'),
-            id: UserDetailsTabTypeEnum.BILLING,
-            href: ROUTES.USER.BILLING.LIST.FORMAT(paramUserName),
-        },
         {
             label: t('users.projects'),
             id: UserDetailsTabTypeEnum.PROJECTS,
             href: ROUTES.USER.PROJECTS.FORMAT(paramUserName),
+        },
+        process.env.UI_VERSION === 'sky' && {
+            label: t('billing.title'),
+            id: UserDetailsTabTypeEnum.BILLING,
+            href: ROUTES.USER.BILLING.LIST.FORMAT(paramUserName),
         },
     ].filter(Boolean);
 
