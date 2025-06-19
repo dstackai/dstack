c10adfbcb3398c4141fb1625be3a47608a2abd1e
jvstme <36324149+jvstme@users.noreply.github.com>
2025-06-19 18:43:11 +0000

    Fix building `dstack` package (#2823)

--- src/dstack/version.py
+++ src/dstack/version.py
@@ -1,3 +1,8 @@
+# WARNING:
+# This file is overwritten when building the dstack package in CI.
+# If you are adding, removing, or renaming variables,
+# remember to update and test the release.yml workflow.
+
 __version__ = "0.0.0"
 __is_release__ = False
 base_image = "0.10"
