2aeab47ec3add89711335ba9f427a6e323b2d48f
Oleg Vavilov <vavilovolegik@gmail.com>
2025-07-01 23:36:06 +0300

    Test commit

--- frontend/src/pages/Fleets/Details/index.tsx
+++ frontend/src/pages/Fleets/Details/index.tsx
@@ -35,10 +35,9 @@ export const FleetDetails: React.FC = () => {
 
     const { data, isLoading } = useGetFleetDetailsQuery(
         {
-        projectName:
-            paramProjectName,
-        fleetId:
-        paramFleetId,
+            projectName: paramProjectName,
+            fleetId:
+            paramFleetId,
         },
         {
             refetchOnMountOrArgChange: true,
