3bd58a3b9fa32a2cb2fa4b97e776eb48553bc00a
Oleg Vavilov <vavilovolegik@gmail.com>
2025-07-01 23:08:05 +0300

    test

--- frontend/webpack/dev.js
+++ frontend/webpack/dev.js
@@ -1,13 +1,13 @@
-const webpack = require("webpack");
-const {join} = require('path');
-const {srcDir} = require('./env');
+const webpack = require('webpack');
+const { join } = require('path');
+const { srcDir } = require('./env');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 const port = 3000;
 
 module.exports = {
-    mode: "development",
+    mode: 'development',
     entry: [
         `webpack-dev-server/client?http://localhost:${port}`, // bundle the client for webpack-dev-server and connect to the provided endpoint
         join(srcDir, 'index.tsx'), // the entry point of our App
@@ -18,27 +18,30 @@ module.exports = {
         hot: true, // enable HMR on the server
         historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
         headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
         },
         proxy: [
             {
-                context: ["/api"],
+                context: ['/api'],
                 changeOrigin: true,
-                target: 'http://127.0.0.1:8000',
+                // target: 'http://127.0.0.1:8000',
+                target: 'https://sky.dstack.ai/', //0ab1c6ee-3f7b-4839-a259-5e5c6074a97f
+                // target: 'https://sky-stage.dstack.ai/', //29001ee8-897c-47ea-bfec-819f56835ffe
+                // target: 'https://cloud.dstack.ai/', //0ab1c6ee-3f7b-4839-a259-5e5c6074a97f
                 logLevel: 'debug',
-            }
+            },
         ],
         client: {
             overlay: {
                 runtimeErrors: (error) => {
-                    return !error.message.includes("ResizeObserver loop completed with undelivered notifications");
+                    return !error.message.includes('ResizeObserver loop completed with undelivered notifications');
                 },
             },
-        }
+        },
     },
-    devtool: "cheap-module-source-map",
+    devtool: 'cheap-module-source-map',
     plugins: [
         new webpack.HotModuleReplacementPlugin(), // enable HMR globally
         new ReactRefreshWebpackPlugin(),
@@ -55,6 +58,6 @@ module.exports = {
             allowAsyncCycles: false,
             // set the current working directory for displaying module paths
             cwd: process.cwd(),
-        })
+        }),
     ],
 };
