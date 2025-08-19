9a1557caec8164fc6ee6b4aac5e877d723d93d22
Oleg <vavilovolegik@gmail.com>
2025-08-19 23:19:13 +0300

    The logs section is too short in the UI  (#2989)
    * The logs section is too short in the UI #2961
    
    * The logs section is too short in the UI #2961

--- frontend/src/pages/Runs/index.ts
+++ frontend/src/pages/Runs/index.ts
@@ -2,5 +2,5 @@ export { RunList } from './List';
 export { RunDetailsPage } from './Details';
 export { RunDetails } from './Details/RunDetails';
 export { JobMetrics } from './Details/Jobs/Metrics';
-export { Logs } from './Details/Logs';
+export { JobLogs } from './Details/Logs';
 export { Artifacts } from './Details/Artifacts';
