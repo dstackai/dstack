4bfdc63af093bb50a75d3359959355512c78799d
Jvst Me <git@jvst.me>
2025-08-19 15:00:17 +0200

    CI refactoring
    - Split GitHub Actions workflows into reusable
      workflows to avoid duplication.
    - Split some jobs into two stages: building an
      artifact and uploading it. This allows to upload
      artifacts only after all were successfully
      built. Also allows to test building artifacts in
      PRs from forks, where credentials for uploading
      are not available.

--- src/dstack/version.py
+++ src/dstack/version.py
@@ -1,7 +1,7 @@
 # WARNING:
 # This file is overwritten when building the dstack package in CI.
-# If you are adding, removing, or renaming variables,
-# remember to update and test the release.yml workflow.
+# If you are making any changes,
+# remember to update and test the build-artifacts.yml workflow.
 
 __version__ = "0.0.0"
 __is_release__ = False
