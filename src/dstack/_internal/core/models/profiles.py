f490d85beb95d1a9fe39168af219afd56b8a4817
Victor Skvortsov <vds003@gmail.com>
2025-10-06 15:30:24 +0500

    Fix idle duration: off and forbid negative durations (#3151)
    * Fix bool parsing for idle_duration
    
    * Forbid negative durations

--- src/dstack/_internal/core/models/profiles.py
+++ src/dstack/_internal/core/models/profiles.py
@@ -80,14 +80,21 @@ def parse_stop_duration(
 def parse_off_duration(v: Optional[Union[int, str, bool]]) -> Optional[Union[Literal["off"], int]]:
     if v == "off" or v is False:
         return "off"
+    if v is True or v is None:
+        return None
+    duration = parse_duration(v)
+    if duration < 0:
+        raise ValueError("Duration cannot be negative")
+    return duration
+
+
+def parse_idle_duration(v: Optional[Union[int, str, bool]]) -> Optional[int]:
+    # Differs from `parse_off_duration` to accept negative durations as `off`
+    # for backward compatibility.
+    if v == "off" or v is False or v == -1:
+        return -1
     if v is True:
         return None
-    return parse_duration(v)
-
-
-def parse_idle_duration(v: Optional[Union[int, str]]) -> Optional[int]:
-    if v == "off" or v == -1:
-        return -1
     return parse_duration(v)
 
 
