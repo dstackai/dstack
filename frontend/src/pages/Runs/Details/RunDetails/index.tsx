56efbd7a7161cfbac4ee421e090e8aeac4d86e75
Oleg Vavilov <vavilovolegik@gmail.com>
2025-10-14 21:30:26 +0300

    [UI] A prototype of the "Connect" section to show on the running dev environment page (WIP)

--- frontend/src/pages/Runs/Details/RunDetails/index.tsx
+++ frontend/src/pages/Runs/Details/RunDetails/index.tsx
@@ -4,26 +4,9 @@ import { useParams } from 'react-router-dom';
 import { get as _get } from 'lodash';
 import { format } from 'date-fns';
 
-import {
-    Alert,
-    Box,
-    Button,
-    Code,
-    ColumnLayout,
-    Container,
-    ExpandableSection,
-    Header,
-    Loader,
-    NavigateLink,
-    Popover,
-    SpaceBetween,
-    StatusIndicator,
-    Tabs,
-    Wizard,
-} from 'components';
+import { Box, ColumnLayout, Container, Header, Loader, NavigateLink, StatusIndicator } from 'components';
 
 import { DATE_TIME_FORMAT } from 'consts';
-import { copyToClipboard } from 'libs';
 import { getRunError, getRunPriority, getRunStatusMessage, getStatusIconColor, getStatusIconType } from 'libs/run';
 import { ROUTES } from 'routes';
 import { useGetRunQuery } from 'services/run';
@@ -41,11 +24,7 @@ import {
     getRunListItemSpot,
 } from '../../List/helpers';
 import { JobList } from '../Jobs/List';
-
-import styles from './styles.module.scss';
-
-const UvInstallCommand = 'uv tool install dstack -U';
-const PipInstallCommand = 'pip install dstack -U';
+import { ConnectToRunWithDevEnvConfiguration } from './ConnectToRunWithDevEnvConfiguration';
 
 export const RunDetails = () => {
     const { t } = useTranslation();
@@ -60,28 +39,6 @@ export const RunDetails = () => {
 
     const serviceUrl = runData ? getRunListItemServiceUrl(runData) : null;
 
-    const getAttachCommand = (runData: IRun) => {
-        const attachCommand = `dstack attach ${runData.run_spec.run_name}`;
-
-        const copyAttachCommand = () => {
-            copyToClipboard(attachCommand);
-        };
-
-        return [attachCommand, copyAttachCommand] as const;
-    };
-
-    const getSSHCommand = (runData: IRun) => {
-        const sshCommand = `ssh ${runData.run_spec.run_name}`;
-
-        const copySSHCommand = () => {
-            copyToClipboard(sshCommand);
-        };
-
-        return [sshCommand, copySSHCommand] as const;
-    };
-
-    const [activeStepIndex, setActiveStepIndex] = React.useState(0);
-
     if (isLoadingRun)
         return (
             <Container>
@@ -91,9 +48,6 @@ export const RunDetails = () => {
 
     if (!runData) return null;
 
-    const [attachCommand, copyAttachCommand] = getAttachCommand(runData);
-    const [sshCommand, copySSHCommand] = getSSHCommand(runData);
-
     const status = finishedRunStatuses.includes(runData.status)
         ? (runData.latest_job_submission?.status ?? runData.status)
         : runData.status;
@@ -101,8 +55,6 @@ export const RunDetails = () => {
     const terminationReason = finishedRunStatuses.includes(runData.status)
         ? runData.latest_job_submission?.termination_reason
         : null;
-
-    const openInIDEUrl = `${runData.run_spec.configuration.ide}://vscode-remote/ssh-remote+${runData.run_spec.run_name}/${runData.run_spec.working_dir || 'workflow'}`;
 
     return (
         <>
@@ -220,202 +172,7 @@ export const RunDetails = () => {
             </Container>
 
             {runData.run_spec.configuration.type === 'dev-environment' && !runIsStopped(runData.status) && (
-                <Container>
-                    <Header variant="h2">Connect</Header>
-
-                    {runData.status === 'running' && (
-                        <Wizard
-                            i18nStrings={{
-                                stepNumberLabel: (stepNumber) => `Step ${stepNumber}`,
-                                collapsedStepsLabel: (stepNumber, stepsCount) => `Step ${stepNumber} of ${stepsCount}`,
-                                skipToButtonLabel: (step) => `Skip to ${step.title}`,
-                                navigationAriaLabel: 'Steps',
-                                // cancelButton: "Cancel",
-                                previousButton: 'Previous',
-                                nextButton: 'Next',
-                                optional: 'required',
-                            }}
-                            onNavigate={({ detail }) => setActiveStepIndex(detail.requestedStepIndex)}
-                            activeStepIndex={activeStepIndex}
-                            onSubmit={() => window.open(openInIDEUrl, '_blank')}
-                            submitButtonText="Open in VS Code"
-                            allowSkipTo={true}
-                            steps={[
-                                {
-                                    title: 'Attach',
-                                    content: (
-                                        <SpaceBetween size="s">
-                                            <Box>To access this run, first you need to attach to it.</Box>
-                                            <div className={styles.codeWrapper}>
-                                                <Code className={styles.code}>{attachCommand}</Code>
-
-                                                <div className={styles.copy}>
-                                                    <Popover
-                                                        dismissButton={false}
-                                                        position="top"
-                                                        size="small"
-                                                        triggerType="custom"
-                                                        content={
-                                                            <StatusIndicator type="success">
-                                                                {t('common.copied')}
-                                                            </StatusIndicator>
-                                                        }
-                                                    >
-                                                        <Button
-                                                            formAction="none"
-                                                            iconName="copy"
-                                                            variant="normal"
-                                                            onClick={copyAttachCommand}
-                                                        />
-                                                    </Popover>
-                                                </div>
-                                            </div>
-
-                                            <ExpandableSection headerText="No CLI installed?">
-                                                <SpaceBetween size="s">
-                                                    <Box />
-                                                    <Box>To use dstack, install the CLI on your local machine.</Box>
-
-                                                    <Tabs
-                                                        variant="container"
-                                                        tabs={[
-                                                            {
-                                                                label: 'uv',
-                                                                id: 'uv',
-                                                                content: (
-                                                                    <>
-                                                                        <div className={styles.codeWrapper}>
-                                                                            <Code className={styles.code}>
-                                                                                {UvInstallCommand}
-                                                                            </Code>
-
-                                                                            <div className={styles.copy}>
-                                                                                <Popover
-                                                                                    dismissButton={false}
-                                                                                    position="top"
-                                                                                    size="small"
-                                                                                    triggerType="custom"
-                                                                                    content={
-                                                                                        <StatusIndicator type="success">
-                                                                                            {t('common.copied')}
-                                                                                        </StatusIndicator>
-                                                                                    }
-                                                                                >
-                                                                                    <Button
-                                                                                        formAction="none"
-                                                                                        iconName="copy"
-                                                                                        variant="normal"
-                                                                                        onClick={() =>
-                                                                                            copyToClipboard(UvInstallCommand)
-                                                                                        }
-                                                                                    />
-                                                                                </Popover>
-                                                                            </div>
-                                                                        </div>
-                                                                    </>
-                                                                ),
-                                                            },
-                                                            {
-                                                                label: 'pip',
-                                                                id: 'pip',
-                                                                content: (
-                                                                    <>
-                                                                        <div className={styles.codeWrapper}>
-                                                                            <Code className={styles.code}>
-                                                                                {PipInstallCommand}
-                                                                            </Code>
-
-                                                                            <div className={styles.copy}>
-                                                                                <Popover
-                                                                                    dismissButton={false}
-                                                                                    position="top"
-                                                                                    size="small"
-                                                                                    triggerType="custom"
-                                                                                    content={
-                                                                                        <StatusIndicator type="success">
-                                                                                            {t('common.copied')}
-                                                                                        </StatusIndicator>
-                                                                                    }
-                                                                                >
-                                                                                    <Button
-                                                                                        formAction="none"
-                                                                                        iconName="copy"
-                                                                                        variant="normal"
-                                                                                        onClick={() =>
-                                                                                            copyToClipboard(PipInstallCommand)
-                                                                                        }
-                                                                                    />
-                                                                                </Popover>
-                                                                            </div>
-                                                                        </div>
-                                                                    </>
-                                                                ),
-                                                            },
-                                                        ]}
-                                                    />
-                                                </SpaceBetween>
-                                            </ExpandableSection>
-                                        </SpaceBetween>
-                                    ),
-                                    isOptional: true,
-                                },
-                                {
-                                    title: 'Open',
-                                    description: 'After the CLI is attached, you can open the dev environment in VS Code.',
-                                    content: (
-                                        <SpaceBetween size="s">
-                                            <Button
-                                                variant="primary"
-                                                external={true}
-                                                onClick={() => window.open(openInIDEUrl, '_blank')}
-                                            >
-                                                Open in VS Code
-                                            </Button>
-
-                                            <ExpandableSection headerText="Need plain SSH?">
-                                                <SpaceBetween size="s">
-                                                    <Box />
-                                                    <div className={styles.codeWrapper}>
-                                                        <Code className={styles.code}>{sshCommand}</Code>
-
-                                                        <div className={styles.copy}>
-                                                            <Popover
-                                                                dismissButton={false}
-                                                                position="top"
-                                                                size="small"
-                                                                triggerType="custom"
-                                                                content={
-                                                                    <StatusIndicator type="success">
-                                                                        {t('common.copied')}
-                                                                    </StatusIndicator>
-                                                                }
-                                                            >
-                                                                <Button
-                                                                    formAction="none"
-                                                                    iconName="copy"
-                                                                    variant="normal"
-                                                                    onClick={() => copySSHCommand()}
-                                                                />
-                                                            </Popover>
-                                                        </div>
-                                                    </div>
-                                                </SpaceBetween>
-                                            </ExpandableSection>
-                                        </SpaceBetween>
-                                    ),
-                                    isOptional: true,
-                                },
-                            ]}
-                        />
-                    )}
-
-                    {runData.status === 'running' && (
-                        <SpaceBetween size="s">
-                            <Box />
-                            <Alert type="info">Waiting for the run to start.</Alert>
-                        </SpaceBetween>
-                    )}
-                </Container>
+                <ConnectToRunWithDevEnvConfiguration run={runData} />
             )}
 
             {runData.jobs.length > 1 && (
