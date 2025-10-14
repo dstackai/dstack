9b3ff0236fa107e6026bc22986756a4f745b1791
peterschmidt85 <andrey.cheptsov@gmail.com>
2025-10-14 08:01:59 -0700

    [Internal]: Replace Instance.termination_reason values with codes #3182
    Review

--- src/dstack/_internal/server/models.py
+++ src/dstack/_internal/server/models.py
@@ -614,6 +614,7 @@ class InstanceModel(BaseModel):
 
     # instance termination handling
     termination_deadline: Mapped[Optional[datetime]] = mapped_column(NaiveDateTime)
+    # TODO: Migrate to EnumAsString(InstanceTerminationReason, 100) after enough releases to ensure backward compatibility
     termination_reason: Mapped[Optional[str]] = mapped_column(String(4000))
     termination_reason_message: Mapped[Optional[str]] = mapped_column(String(4000))
     # Deprecated since 0.19.22, not used
