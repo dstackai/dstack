bf45abaab8c836aef691ddadcb86f9d4a1f1ad65
Dmitry Meyer <me@undef.im>
2025-12-08 13:58:24 +0000

    Add tini to server Docker images
    Closes: https://github.com/dstackai/dstack/issues/3302

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -10,7 +10,7 @@ fi
 DB_PATH="${HOME}/.dstack/server/data/sqlite.db"
 mkdir -p "$(dirname "$DB_PATH")"
 if [[ -z "${LITESTREAM_REPLICA_URL}" ]]; then
-  dstack server --host 0.0.0.0
+  exec dstack server --host 0.0.0.0
 else
   if [[ ! -f "$DB_PATH" ]]; then
     echo "Attempting Litestream restore..."
@@ -23,5 +23,5 @@ else
       fi
     fi
   fi
-  litestream replicate -exec "dstack server --host 0.0.0.0" "$DB_PATH" "$LITESTREAM_REPLICA_URL"
+  exec litestream replicate -exec "dstack server --host 0.0.0.0" "$DB_PATH" "$LITESTREAM_REPLICA_URL"
 fi
