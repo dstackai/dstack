8108d0cf7ced57e384799c7a9bfc910e0c936d85
Victor Skvortsov <vds003@gmail.com>
2026-04-08 15:18:07 +0500

    Drop skipped test

--- src/tests/_internal/server/services/test_config.py
+++ src/tests/_internal/server/services/test_config.py
@@ -6,11 +6,6 @@ import yaml
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from dstack._internal.core.backends.azure.models import (
-    AzureBackendConfigWithCreds,
-    AzureDefaultCreds,
-)
-from dstack._internal.core.models.backends.base import BackendType
 from dstack._internal.server import settings
 from dstack._internal.server.models import BackendModel, ProjectModel
 from dstack._internal.server.services.config import ServerConfigManager
@@ -21,47 +16,6 @@ from dstack._internal.server.testing.common import (
 
 
 class TestServerConfigManager:
-    class TestInitConfig:
-        @pytest.mark.asyncio
-        @pytest.mark.parametrize("test_db", ["sqlite", "postgres"], indirect=True)
-        @pytest.mark.skip(reason="Backends auto init is currently disabled")
-        async def test_inits_backend(self, test_db, session: AsyncSession, tmp_path: Path):
-            await create_project(session=session, name="main")
-            config_filepath = tmp_path / "config.yml"
-            with (
-                patch.object(settings, "SERVER_CONFIG_FILE_PATH", config_filepath),
-                patch(
-                    "dstack._internal.server.services.backends.list_available_backend_types"
-                ) as list_available_backend_types_mock,
-                patch(
-                    "dstack._internal.server.services.backends.get_configurator"
-                ) as get_configurator_mock,
-                patch(
-                    "dstack._internal.server.services.backends.create_backend"
-                ) as create_backend_mock,
-            ):
-                list_available_backend_types_mock.return_value = [BackendType.AZURE]
-                default_config = AzureBackendConfigWithCreds(
-                    tenant_id="test_tenant",
-                    subscription_id="test_subscription",
-                    locations=["westeurope"],
-                    creds=AzureDefaultCreds(),
-                )
-                get_configurator_mock.return_value.get_default_configs.return_value = [
-                    default_config
-                ]
-                manager = ServerConfigManager()
-                await manager.init_config(session)
-                list_available_backend_types_mock.assert_called()
-                get_configurator_mock.assert_called()
-                create_backend_mock.assert_called()
-                assert manager.config.projects[0].backends[0] == AzureBackendConfigWithCreds(
-                    tenant_id="test_tenant",
-                    subscription_id="test_subscription",
-                    regions=["westeurope"],
-                    creds=AzureDefaultCreds(),
-                )
-
     class TestApplyConfig:
         @pytest.mark.asyncio
         @pytest.mark.parametrize("test_db", ["sqlite", "postgres"], indirect=True)
