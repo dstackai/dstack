14bd27af6713261b2cf58e76855a06b9b10985b3
Victor Skvortsov <vds003@gmail.com>
2026-06-16 14:16:25 +0500

    Restore tests update

--- src/tests/_internal/server/routers/test_runs.py
+++ src/tests/_internal/server/routers/test_runs.py
@@ -390,7 +390,7 @@ def get_dev_env_run_dict(
                 " && tail -f /dev/null"
             ),
         ]
-        image_name = f"dstackai/base:{settings.DSTACK_DOCKER_BASE_IMAGE_VERSION}-base-ubuntu{settings.DSTACK_DOCKER_BASE_IMAGE_UBUNTU_VERSION}"
+        image_name = "dstackai/base:0.13-base-ubuntu22.04"
 
     return {
         "id": run_id,
