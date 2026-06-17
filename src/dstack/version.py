0ac340990edef63d5fcb4695013389287d1974f9
Victor Skvortsov <vds003@gmail.com>
2026-06-17 14:33:26 +0500

    Bump base images versions to 0.14

--- src/dstack/version.py
+++ src/dstack/version.py
@@ -5,6 +5,6 @@
 
 __version__ = "0.0.0"
 __is_release__ = False
-docker_base_image = "0.13"
-docker_base_image_ubuntu_version = "22.04"
-vm_base_image = "0.13"
+docker_base_image = "0.14"
+docker_base_image_ubuntu_version = "24.04"
+vm_base_image = "0.14"
