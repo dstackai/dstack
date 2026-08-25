a62ef35a5f0168b85e70b1475aacd07586d7bfb3
Jvst Me <git@jvst.me>
2026-08-25 22:07:37 +0200

    Fix events client backward compatibility

--- src/dstack/_internal/core/compatibility/events.py
+++ src/dstack/_internal/core/compatibility/events.py
@@ -3,5 +3,7 @@ from dstack._internal.server.schemas.events import ListEventsRequest
 
 
 def get_list_events_excludes(request: ListEventsRequest) -> IncludeExcludeDictType:
-    list_gpus_excludes: IncludeExcludeDictType = {}
-    return list_gpus_excludes
+    list_events_excludes: IncludeExcludeDictType = {}
+    if request.target_presets is None:
+        list_events_excludes["target_presets"] = True
+    return list_events_excludes
