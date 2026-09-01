eb15041d648f3615f989b3499f8bd230a9324e0d
jvstme <36324149+jvstme@users.noreply.github.com>
2026-09-01 14:37:44 +0000

    Tune Nginx config on the gateway (#4237)
    Bring our own config, based on the default Ubuntu
    config, but with increased `worker_rlimit_nofile`,
    `worker_connections`,
    `server_names_hash_bucket_size`.

--- src/tests/_internal/proxy/gateway/test_app.py
+++ src/tests/_internal/proxy/gateway/test_app.py
@@ -21,12 +21,14 @@ async def test_lifespan(tmp_path: Path, system_mocks: Mocks) -> None:
         make_service("test-proj", "test-run", domain="test-run.gtw.test", https=True)
     )
     nginx_dir = tmp_path / "nginx"
-    nginx_dir.mkdir()
-    app = make_app(repo=repo, nginx=Nginx(conf_dir=nginx_dir))
+    conf_dir = nginx_dir / "sites-enabled"
+    conf_dir.mkdir(parents=True)
+    app = make_app(repo=repo, nginx=Nginx(nginx_dir=nginx_dir))
     async with lifespan(app):
-        assert (nginx_dir / "00-log-format.conf").exists()
-        assert (nginx_dir / "443-gateway.gtw.test.conf").exists()
-        assert (nginx_dir / "443-test-run.gtw.test.conf").exists()
+        assert (conf_dir / "00-log-format.conf").exists()
+        assert (conf_dir / "443-gateway.gtw.test.conf").exists()
+        assert (conf_dir / "443-test-run.gtw.test.conf").exists()
+        assert (nginx_dir / "nginx.conf").exists()
         assert system_mocks.open_conn.call_count == 1
         assert system_mocks.close_conn.call_count == 0
     assert system_mocks.close_conn.call_count == 1
