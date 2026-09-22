7281fa7c28f4bcc4ec5a1aa460379fc88e3cb0d9
Andrey Cheptsov <andrey.cheptsov@github.com>
2026-09-22 09:03:06 +0200

    [Website] Enable React Router transition flag

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -9,6 +9,6 @@ import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
   </React.StrictMode>,
 );
